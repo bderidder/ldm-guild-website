2e883e1ef65d9d27b07ccbcb422b0fc19162f519
Bavo De Ridder <bavo@coderspotting.org>
2013-12-09 12:05:13 +0100

    Removed obsolete testing code for Doctrine

--- src/LaDanse/SiteBundle/Controller/CreateEventController.php
+++ src/LaDanse/SiteBundle/Controller/CreateEventController.php
@@ -10,8 +10,6 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 
-use LaDanse\DomainBundle\Entity\Product;
-
 /**
  * @Route("/Events/Create")
 */
@@ -23,13 +21,5 @@ class CreateEventController extends Controller
      */
     public function indexAction(Request $request)
     {
-    	$product = new Product();
-    	$product->setName('A Foo Bar');
-    	$product->setPrice('19.99');
-    	$product->setDescription('Lorem ipsum dolor');
-
-    	$em = $this->getDoctrine()->getManager();
-    	$em->persist($product);
-    	$em->flush();
     }
 }
