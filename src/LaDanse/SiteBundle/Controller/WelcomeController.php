63e8ee34b2c3cf51e94d2b2b28b474bc5e9044cd
Bavo De Ridder <bavo@coderspotting.org>
2013-12-09 21:30:33 +0100

    Added LaDanseSite routing

--- src/LaDanse/SiteBundle/Controller/WelcomeController.php
+++ src/LaDanse/SiteBundle/Controller/WelcomeController.php
@@ -3,11 +3,16 @@
 namespace LaDanse\SiteBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+
+use LaDanse\SiteBundle\Security\AuthenticationContext;
 
 class WelcomeController extends Controller
 {
-    public function indexAction()
+    public function indexAction(Request $request)
     {
+    	$authContext = new AuthenticationContext($this->get('LaDanse.ContainerInjector'), $request);
+
         return $this->render('LaDanseSiteBundle::index.html.twig');
     }
 }
