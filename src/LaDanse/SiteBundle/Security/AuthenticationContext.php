39df624303d245fb7572fba27b64269105b559cb
Bavo De Ridder <bavo@coderspotting.org>
2013-12-13 17:42:12 +0100

    Updated so that the session is used, switching is possible

--- src/LaDanse/SiteBundle/Security/AuthenticationContext.php
+++ src/LaDanse/SiteBundle/Security/AuthenticationContext.php
@@ -5,6 +5,7 @@ namespace LaDanse\SiteBundle\Security;
 use Symfony\Component\DependencyInjection\ContainerInterface as Container;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Session\Session;
 
 use Doctrine\Bundle\DoctrineBundle\Registry;
 
@@ -14,18 +15,45 @@ use LaDanse\DomainBundle\Entity\Account;
 
 class AuthenticationContext extends ContainerAwareClass
 {
-	private $account;
+	private $account = NULL;
 
 	public function __construct(ContainerInjector $ci, Request $request)
 	{
 		parent::__construct($ci);
 
-		$this->account = $this->getDoctrine()->getRepository('LaDanseDomainBundle:Account')->find(1);
+        $session = new Session();
+        
+        $id = $session->get('LaDanseAuthenticationContextId');
+
+        if ($id)
+        {
+            $this->switchUser($request, $id);
+        }
+    }
+
+    public function switchUser(Request $request, $id)
+    {
+        $this->account = $this->getDoctrine()->getRepository('LaDanseDomainBundle:Account')->find($id);
+
+        $session = new Session();
+        $session->set('LaDanseAuthenticationContextId', $id);
     }
 
     public function isAuthenticated()
     {
     	return !is_null($this->account);
+    }
+
+    public function getId()
+    {
+        if ($this->isAuthenticated())
+        {
+            return $this->account->getId();
+        }
+        else
+        {
+            return -1;
+        }
     }
 
     public function getAccount()
