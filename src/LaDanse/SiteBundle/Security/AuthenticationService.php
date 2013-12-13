399f6e66b039ff859ffb2a45ae7f797173db0f50
Bavo De Ridder <bavo@coderspotting.org>
2013-12-13 22:11:28 +0100

    Initial revision

---
+++ src/LaDanse/SiteBundle/Security/AuthenticationService.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace LaDanse\SiteBundle\Security;
+
+use Symfony\Component\DependencyInjection\ContainerInterface as Container;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Session\Session;
+
+use Doctrine\Bundle\DoctrineBundle\Registry;
+
+use LaDanse\CommonBundle\Helper\ContainerAwareClass;
+
+use LaDanse\DomainBundle\Entity\Account;
+
+class AuthenticationService extends ContainerAwareClass
+{
+	public function __construct(Container $container)
+	{
+		parent::__construct($container);
+    }
+
+    public function getCurrentContext()
+    {
+        return $this->getContainer()->get('LaDanse.AuthenticationContext');
+    }
+}
