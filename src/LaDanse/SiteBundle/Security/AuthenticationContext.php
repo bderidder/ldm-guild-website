21bb69782d6e95b0a1b802b9fcb6dfe5ccdaef16
Bavo De Ridder <bavo@coderspotting.org>
2013-12-13 22:14:11 +0100

    Switched to a full Symfony service, state is stored on the session

--- src/LaDanse/SiteBundle/Security/AuthenticationContext.php
+++ src/LaDanse/SiteBundle/Security/AuthenticationContext.php
@@ -3,40 +3,43 @@
 namespace LaDanse\SiteBundle\Security;
 
 use Symfony\Component\DependencyInjection\ContainerInterface as Container;
-use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Session\Session;
 
 use Doctrine\Bundle\DoctrineBundle\Registry;
 
-use LaDanse\CommonBundle\Helper\ContainerInjector;
 use LaDanse\CommonBundle\Helper\ContainerAwareClass;
+
 use LaDanse\DomainBundle\Entity\Account;
 
 class AuthenticationContext extends ContainerAwareClass
 {
 	private $account = NULL;
 
-	public function __construct(ContainerInjector $ci, Request $request)
+	public function __construct(Container $container)
 	{
-		parent::__construct($ci);
+		parent::__construct($container);
 
-        $session = new Session();
-        
-        $id = $session->get('LaDanseAuthenticationContextId');
+        $id = $this->getSession()->get('LaDanseAuthenticationContextId');
 
-        if ($id)
+        if (!is_null($id))
         {
-            $this->switchUser($request, $id);
+            $this->account = $this->getDoctrine()->getRepository('LaDanseDomainBundle:Account')->find($id);
         }
     }
 
-    public function switchUser(Request $request, $id)
+    public function login($id)
     {
         $this->account = $this->getDoctrine()->getRepository('LaDanseDomainBundle:Account')->find($id);
 
-        $session = new Session();
-        $session->set('LaDanseAuthenticationContextId', $id);
+        $this->getSession()->set('LaDanseAuthenticationContextId', $id);
+    }
+
+    public function logout()
+    {
+        $this->account = NULL;
+
+        $this->getSession()->set('LaDanseAuthenticationContextId', NULL);           
     }
 
     public function isAuthenticated()
@@ -59,5 +62,23 @@ class AuthenticationContext extends ContainerAwareClass
     public function getAccount()
     {
     	return $this->account;
+    }
+
+    private function getSession()
+    {
+        $requestStack = $this->getContainer()->get('request_stack');
+        $request = $requestStack->getCurrentRequest();
+
+        if (!$request->hasSession())
+        {
+            $session = new Session();
+            $request->setSession($session);
+
+            return $session;
+        }
+        else
+        {
+            return $request->getSession();
+        }
     }
 }
