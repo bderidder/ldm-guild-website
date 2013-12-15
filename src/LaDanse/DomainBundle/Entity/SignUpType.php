c093b4968676f0d17e09ca641b60828733950c80
Bavo De Ridder <bavo@coderspotting.org>
2013-12-15 15:52:50 +0100

    Initial revision

---
+++ src/LaDanse/DomainBundle/Entity/SignUpType.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace LaDanse\DomainBundle\Entity;
+
+use LaDanse\DomainBundle\Entity\SignUp;
+
+class SignUpType
+{
+    const WILLCOME      = "WillCome";
+    const MIGHTCOME     = "MightCome";
+    const ABSENCE       = "Absence";
+}
