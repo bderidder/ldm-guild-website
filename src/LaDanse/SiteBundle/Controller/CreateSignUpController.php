6d1dd001f81d3dd30e5e7d38116ccc4f20b17b93
Bavo De Ridder <bavo@coderspotting.org>
2013-12-15 10:31:15 +0100

    Added first (temporary) code to create a signup

--- src/LaDanse/SiteBundle/Controller/CreateSignUpController.php
+++ src/LaDanse/SiteBundle/Controller/CreateSignUpController.php
@@ -11,16 +11,27 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 
 use LaDanse\CommonBundle\Helper\LaDanseController;
 
+use LaDanse\SiteBundle\Security\AuthenticationContext;
+
+use LaDanse\SiteBundle\Form\Model\SignUpFormModel;
+use LaDanse\SiteBundle\Form\Type\SignUpFormType;
+
+use LaDanse\DomainBundle\Entity\SignUp;
+use LaDanse\DomainBundle\Entity\ForRole;
+use LaDanse\DomainBundle\Entity\Role;
+
 /**
- * @Route("/signups/create")
+ * @Route("/event/{id}/signup")
 */
 class CreateSignUpController extends LaDanseController
 {
+    const EVENT_REPOSITORY = 'LaDanseDomainBundle:Event';
+
 	/**
      * @Route("/", name="createSignUpIndex")
      * @Template("LaDanseSiteBundle::createSignUp.html.twig")
      */
-    public function indexAction(Request $request)
+    public function indexAction(Request $request, $id)
     {
     	$authContext = $this->getAuthenticationService()->getCurrentContext();
 
@@ -28,5 +39,56 @@ class CreateSignUpController extends LaDanseController
     	{
     		return $this->redirect($this->generateUrl('eventsIndex'));
     	}
+
+        $em = $this->getDoctrine();
+        $repository = $em->getRepository(self::EVENT_REPOSITORY);
+        $event = $repository->find($id);
+
+        if (null === $event)
+        {
+            return $this->redirect($this->generateUrl('viewEventsIndex'));
+        } 
+
+        if ($this->isCurrentUserSigned($event))
+        {
+            return $this->redirect($this->generateUrl('viewEventsIndex'));
+        }       
+
+        $formModel = new SignUpFormModel();
+
+        $form = $this->createForm(new SignUpFormType(), $formModel);
+
+        $form->handleRequest($request);
+
+        if ($form->isValid())
+        {
+            $this->persistSignUp($authContext, $id);
+
+            return $this->redirect($this->generateUrl('viewEventsIndex'));
+        }
+        else
+        {
+            return array('event' => $event, 'form' => $form->createView());            
+        }
+    }
+
+    private function persistSignUp(AuthenticationContext $authContext, $eventId)
+    {
+        $em = $this->getDoctrine()->getEntityManager();
+
+        $repository = $em->getRepository(self::EVENT_REPOSITORY);
+        $event = $repository->find($eventId);
+
+        $signUp = new SignUp();
+        $signUp->setEvent($event);
+        $signUp->setAccount($authContext->getAccount());
+
+        $em->persist($signUp);
+        $em->flush();
+    }
+
+    private function isCurrentUserSigned($event)
+    {
+
     }
 }
