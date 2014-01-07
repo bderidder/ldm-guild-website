95410ba68c2df9526d454033d62d8261f5fda14b
Bavo De Ridder <bavo@coderspotting.org>
2014-01-07 22:13:51 +0100

    Initial revision

---
+++ src/CoderSpotting/Bundle/ToastMessageBundle/CoderSpottingToastMessageBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace CoderSpotting\Bundle\ToastMessageBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class CoderSpottingToastMessageBundle extends Bundle
+{
+}
---
+++ src/CoderSpotting/Bundle/ToastMessageBundle/Twig/TwigToastExtension.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace CoderSpotting\Bundle\ToastMessageBundle\Twig;
+
+use Symfony\Component\DependencyInjection\ContainerInterface;
+
+use Symfony\Component\HttpFoundation\Session\Session;
+
+class TwigToastExtension extends \Twig_Extension
+{
+	private $container;
+
+	public function __construct(ContainerInterface $container)
+	{
+    	$this->container = $container;
+	}
+
+    public function getFunctions()
+    {
+        return array(
+            new \Twig_SimpleFunction('toasts', array($this, 'toastsFunction'), array('is_safe' => array('html'))),
+        );
+    }
+
+    public function toastsFunction()
+    {
+        $toastService = $this->container->get('CoderSpotting.ToastMessage');
+
+        $toasts = $toastService->getToasts();
+
+        if (!isset($toasts))
+            return;
+
+        $html = '';
+
+        foreach($toasts as $toastMessage)
+        {
+            $html .= "$().toastmessage('showNoticeToast', '" . $toastMessage . "');";
+        }
+
+        $toastService->resetToasts();
+
+        return $html;
+    }
+
+    public function getName()
+    {
+        return 'CoderSpotting_ToastMessage_Extension';
+    }
+}
