66ca14ccadd3e369d1f1dc1a3cff9e4d626ad5ab
Bavo De Ridder <bavo@coderspotting.org>
2014-01-27 21:13:40 +0100

    Various changes to upgrade the code to production level

--- src/LaDanse/SiteBundle/Controller/CalendarController.php
+++ src/LaDanse/SiteBundle/Controller/CalendarController.php
@@ -29,7 +29,11 @@ class CalendarController extends LaDanseController
     {
     	$authContext = $this->getAuthenticationService()->getCurrentContext();
 
-        $startDate = \DateTime::createFromFormat("d/m/Y", "19/01/2014");
+        // fetch the Monday we should start with
+        $startDate = $this->getStartDate();
+
+        // the algoritm below needs to start on the day before, so we substract a day
+        $startDate = $startDate->sub(new \DateInterval("P1D"));
 
         $calendarDates = array();
 
@@ -45,6 +49,10 @@ class CalendarController extends LaDanseController
             $date->add(new \DateInterval("P1D"));
 
             $calendarDateModel = new CalendarDayModel($this->getContainerInjector(), $date);
+
+            if ($i === 0) $calendarDateModel->setShowMonth(true);
+
+            if ($date->format("d") === "01") $calendarDateModel->setShowMonth(true);
 
             $calendarDates[] = $calendarDateModel;
 
@@ -63,14 +71,27 @@ class CalendarController extends LaDanseController
 
                     $eventIndex++;
                 }
+                else
+                {
+                    $eventIndex++;
+                }
             }
 
             $currentDate = $date;
         }
 
-        return $this->render('LaDanseSiteBundle::calendarPartial.html.twig',
+        if ($authContext->isAuthenticated())
+        {
+            return $this->render('LaDanseSiteBundle::calendarPartial.html.twig',
                     array('calendarDays' => $calendarDates)
                 );
+        }
+        else
+        {
+            return $this->render('LaDanseSiteBundle::calendarPartialGuest.html.twig',
+                    array('calendarDays' => $calendarDates)
+                );
+        }
     }
 
     protected function getEvents()
@@ -78,8 +99,8 @@ class CalendarController extends LaDanseController
         $em = $this->getDoctrine()->getManager();
 
         /* @var $query \Doctrine\ORM\Query */
-        $query = $em->createQuery('SELECT e FROM LaDanse\DomainBundle\Entity\Event e WHERE e.inviteTime > :now ORDER BY e.inviteTime ASC');
-        $query->setParameter('now', new \DateTime('now'));
+        $query = $em->createQuery('SELECT e FROM LaDanse\DomainBundle\Entity\Event e WHERE e.inviteTime > :start ORDER BY e.inviteTime ASC');
+        $query->setParameter('start', (new \DateTime('now'))->sub(new \DateInterval("P10D")));
         
         $events = $query->getResult();
 
@@ -91,5 +112,34 @@ class CalendarController extends LaDanseController
         }
 
         return $eventModels;
+    }
+
+    private function getStartDate()
+    {
+        $day = date('w');
+
+        if ($day === 0)
+        {
+            // it's Sunday but in Europe we start our weeks on Monday
+            $day = 7;
+        }
+        
+        // days are not from Monday (1) to Sunday (7)
+
+        if ($day < 3)
+        {
+            // it's monday or tuesday, we need to include the previous week in our
+            // calendar to show the current raid week completely
+
+            $day = $day + 6;
+        }
+        else
+        {
+            $day = $day - 1;
+        }
+
+        $calendarStart = date('d/m/Y', strtotime('-' . $day . ' days'));
+
+        return \DateTime::createFromFormat("d/m/Y", $calendarStart);
     }
 }
