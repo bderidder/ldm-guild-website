1137d1c43176ae871610d271a7c2c128b6d62027
Bavo De Ridder <bavo@coderspotting.org>
2014-03-04 18:26:58 +0100

    Split list and remove actions in their own controllers

---
+++ src/LaDanse/SiteBundle/Controller/Events/RemoveEventController.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace LaDanse\SiteBundle\Controller\Events;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+
+use LaDanse\CommonBundle\Helper\LaDanseController;
+
+use LaDanse\SiteBundle\Model\EventModel;
+
+/**
+ * @Route("/{id}")
+*/
+class RemoveEventController extends LaDanseController
+{
+	const EVENT_REPOSITORY = 'LaDanseDomainBundle:Event';
+
+    /**
+     * @Route("/delete", name="deleteEventIndex")
+     */
+    public function deleteAction($id)
+    {
+    	$em = $this->getDoctrine()->getManager();
+    	$repository = $this->getDoctrine()->getRepository(self::EVENT_REPOSITORY);
+
+        /* @var $repository \Doctrine\ORM\EntityRepository */
+    	$event = $repository->find($id);
+
+        if (null === $event)
+        {
+            $this->getLogger()->warn(__CLASS__ . ' the event does not exist in deleteAction', 
+                array("event" => $id));
+
+            return $this->redirect($this->generateUrl('welcomeIndex'));
+        }
+        else
+        {
+    	   $em->remove($event);
+
+           $this->getLogger()->warn(__CLASS__ . ' removing event in deleteAction', 
+                array("event" => $id));
+
+    	   $em->flush();
+
+           $this->addToast('Event removed');
+
+    	   return $this->redirect($this->generateUrl('welcomeIndex'));
+        }
+    }
+}
