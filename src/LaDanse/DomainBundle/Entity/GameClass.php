417f935a388897ae8ac5e462cde50dfc00cb5ff3
Bavo De Ridder <bavo@coderspotting.org>
2014-03-12 21:54:41 +0100

    Initial revision

---
+++ src/LaDanse/DomainBundle/Entity/GameClass.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace LaDanse\DomainBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="GameClass")
+ */
+class GameClass
+{
+	/**
+     * @ORM\Column(type="integer")
+     * @ORM\Id
+     */
+    protected $id;
+
+    /**
+     * @ORM\Column(type="string", length=20, nullable=false)
+     */
+    protected $name;
+
+    /**
+     * Set id
+     *
+     * @param integer $id
+     * @return GameClass
+     */
+    public function setId($id)
+    {
+        $this->id = $id;
+
+        return $this;
+    }
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set name
+     *
+     * @param string $name
+     * @return GameClass
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * Get name
+     *
+     * @return string 
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+}
