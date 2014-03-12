1ebaea7386ab221782ba53838cd36f6adb6de6c9
Bavo De Ridder <bavo@coderspotting.org>
2014-03-12 21:55:00 +0100

    First version (copy from GuildCharacterService)

---
+++ src/LaDanse/ServicesBundle/Service/GameDataService.php
@@ -0,0 +1,275 @@
+<?php
+
+namespace LaDanse\ServicesBundle\Service;
+
+use Symfony\Component\DependencyInjection\ContainerAware,
+    Symfony\Component\DependencyInjection\ContainerInterface;
+
+use LaDanse\CommonBundle\Helper\LaDanseService;
+
+use LaDanse\DomainBundle\Entity\GameClass,
+    LaDanse\DomainBundle\Entity\GameRace;
+
+class GameDataService extends LaDanseService
+{
+    const SERVICE_NAME = 'LaDanse.GameDataService';
+
+	public function __construct(ContainerInterface $container)
+	{
+		parent::__construct($container);
+	}
+
+    public function getAllRaces()
+    {
+    }
+
+    public function getAllClasses()
+    {
+    }
+
+	public function getClaims($accountId, \DateTime $onDateTime = NULL)
+    {
+    	if ($onDateTime == NULL)
+    	{
+    		// when not set, initialize to right now
+    		$onDateTime = new \DateTime();
+    	}
+
+        $em = $this->getDoctrine()->getManager();
+
+        /* @var $query \Doctrine\ORM\Query */
+        $query = $em->createQuery(
+            $this->createSQLFromTemplate('LaDanseDomainBundle::selectClaimsForAccount.sql.twig'));
+        $query->setParameter('accountId', $accountId);
+        $query->setParameter('onDateTime', $onDateTime);
+        
+        $claims = $query->getResult();
+
+        $claimsModels = array();
+
+        foreach($claims as $claim)
+        {
+            $claimsModels[] = (object)array(
+                "id"          => $claim->getId(),
+                "name"        => $claim->getCharacter()->getName(),
+                "fromTime"    => $claim->getFromTime(),
+                "playsTank"   => $this->containsRole($claim->getRoles(), Role::TANK),
+                "playsHealer" => $this->containsRole($claim->getRoles(), Role::HEALER),
+                "playsDPS"    => $this->containsRole($claim->getRoles(), Role::DPS),
+            );
+        }
+
+        return $claimsModels;
+    }
+
+    public function getAllCharacters(\DateTime $onDateTime = NULL)
+    {
+        if ($onDateTime == NULL)
+        {
+            // when not set, initialize to right now
+            $onDateTime = new \DateTime();
+        }
+
+        $em = $this->getDoctrine()->getManager();
+
+        /* @var $query \Doctrine\ORM\Query */
+        $query = $em->createQuery(
+            $this->createSQLFromTemplate('LaDanseDomainBundle::selectCharactersForAccount.sql.twig'));
+
+        $query->setParameter('onDateTime', $onDateTime);
+
+        $characters = $query->getResult();
+
+        return $this->charactersToDtoArray($characters);
+    }
+
+    public function getUnclaimedCharacters(\DateTime $onDateTime = NULL)
+    {
+        if ($onDateTime == NULL)
+        {
+            // when not set, initialize to right now
+            $onDateTime = new \DateTime();
+        }
+
+        $em = $this->getDoctrine()->getManager();
+
+        /* @var $query \Doctrine\ORM\Query */
+        $query = $em->createQuery(
+            $this->createSQLFromTemplate('LaDanseDomainBundle::selectUnclaimedCharacters.sql.twig'));
+        $query->setParameter('onDateTime', $onDateTime);
+
+        $characters = $query->getResult();
+
+        return $this->charactersToDtoArray($characters);
+    }
+
+    public function endCharacter($characterId)
+    {
+        $em = $this->getDoctrine()->getManager();
+        $repo = $this->getDoctrine()->getRepository("LaDanseDomainBundle:Character");
+
+        $character = $repo->find($characterId);
+
+        $character->setEndTime(new \DateTime());
+
+        $em->flush();
+
+        $this->endClaimsForCharacter($character);
+    }
+
+    public function getActiveClaimsForAccount($account, \DateTime $onDateTime = NULL)
+    {
+
+    }
+
+    public function getActiveClaimsForCharacter($character, \DateTime $onDateTime = NULL)
+    {
+
+    }
+
+    public function createClaim($accountId, $characterId, $playsTank, $playsHealer, $playsDPS)
+    {
+        $onDateTime = new \DateTime();
+
+        $em = $this->getDoctrine()->getManager();
+
+        /* @var $repository \Doctrine\ORM\EntityRepository */
+        $characterRepo = $em->getRepository(Character::REPOSITORY);
+        /* @var $event \LaDanse\DomainBundle\Entity\Character */
+        $character = $characterRepo->find($characterId);
+
+        /* @var $repository \Doctrine\ORM\EntityRepository */
+        $accountRepo = $em->getRepository(Account::REPOSITORY);
+        /* @var $event \LaDanse\DomainBundle\Entity\Character */
+        $account = $accountRepo->find($accountId);
+
+        $claim = new Claim();
+        $claim->setCharacter($character)
+              ->setAccount($account)
+              ->setFromTime($onDateTime);
+
+        if ($playsTank)
+        {
+            $em->persist($this->createPlaysRole($onDateTime, $claim, Role::TANK));
+        }
+        
+        if ($playsHealer)
+        {
+            $em->persist($this->createPlaysRole($onDateTime, $claim, Role::HEALER));
+        }
+
+        if ($playsDPS)
+        {
+            $em->persist($this->createPlaysRole($onDateTime, $claim, Role::DPS));
+        }
+
+        $this->getLogger()->info(__CLASS__ . ' persisting new claim');
+
+        $em->persist($claim);
+        $em->flush();
+    }
+
+    public function endClaim($claimId)
+    {
+        $onDateTime = new \DateTime();
+
+        $em = $this->getDoctrine()->getManager();
+
+        /* @var $repository \Doctrine\ORM\EntityRepository */
+        $repository = $em->getRepository(Claim::REPOSITORY);
+        /* @var $claim \LaDanse\DomainBundle\Entity\Claim */
+        $claim = $repository->find($claimId);
+
+        $claim->setEndTime($onDateTime);
+
+        foreach($claim->getRoles() as $playsRole)
+        {
+            $playsRole->setEndTime($onDateTime);
+        }
+
+        $em->flush();
+    }
+
+    public function endClaimsForCharacter($character)
+    {
+        $onDateTime = new \DateTime();
+
+        $em = $this->getDoctrine()->getManager();
+
+        /* @var $query \Doctrine\ORM\Query */
+        $query = $em->createQuery(
+            $this->createSQLFromTemplate('LaDanseDomainBundle::selectActiveClaimsForCharacter.sql.twig'));
+
+        $query->setParameter('character', $character);
+
+        $claims = $query->getResult();
+
+        foreach($claims as $claim)
+        {
+            $claim->setEndTime($onDateTime);
+
+            foreach($claim->getRoles() as $playsRole)
+            {
+                $playsRole->setEndTime($onDateTime);
+            }
+        }
+
+        $em->flush();
+    }
+
+    public function importCharacter($name)
+    {
+        $em = $this->getDoctrine()->getManager();
+
+        $character = new Character();
+        $character->setName($name);
+        $character->setFromTime(new \DateTime());
+
+        $em->persist($character);
+        $em->flush();
+    }
+
+    protected function charactersToDtoArray($characters)
+    {
+        $charactersDto = array();
+
+        foreach($characters as $character)
+        {
+            $charactersDto[] = $this->characterToDto($character);
+        }
+
+        return $charactersDto;
+    }
+
+    protected function characterToDto($character)
+    {
+        return (object)array(
+            "id"        => $character->getId(),
+            "name"      => $character->getName(),
+            "fromTime"  => $character->getFromTime()
+        );
+    }
+
+    protected function createPlaysRole($onDateTime, $claim, $role)
+    {
+        $playsRole = new PlaysRole();
+        $playsRole->setRole($role)
+                  ->setClaim($claim)
+                  ->setFromTime($onDateTime);
+
+        return $playsRole;
+    }
+
+    protected function containsRole($playsRoles, $role)
+    {
+        foreach($playsRoles as $playsRole)
+        {
+            if ($playsRole->isRole($role))
+            {
+                return true;
+            }
+        }
+
+        return false;
+    }
+}
