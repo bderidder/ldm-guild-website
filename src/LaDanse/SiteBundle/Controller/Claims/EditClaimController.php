b40f3a0605d4ef7d5603e63429f0392b625cdb38
Bavo De Ridder <bavo@coderspotting.org>
2014-03-28 08:13:46 +0100

    Claim edits are now properly saved

--- src/LaDanse/SiteBundle/Controller/Claims/EditClaimController.php
+++ src/LaDanse/SiteBundle/Controller/Claims/EditClaimController.php
@@ -5,16 +5,17 @@ namespace LaDanse\SiteBundle\Controller\Claims;
 use \DateTime;
 
 use Symfony\Component\HttpFoundation\Request,
-    Symfony\Component\HttpFoundation\Response,
-    Symfony\Component\HttpFoundation\JsonResponse;
+    Symfony\Component\HttpFoundation\Response;
 
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 
 use LaDanse\CommonBundle\Helper\LaDanseController;
 
-use LaDanse\SiteBundle\Form\Model\NewClaimFormModel;
-use LaDanse\SiteBundle\Form\Type\NewClaimFormType;
+use LaDanse\SiteBundle\Form\Model\EditClaimFormModel;
+use LaDanse\SiteBundle\Form\Type\EditClaimFormType;
+
+use LaDanse\DomainBundle\Entity\Role;
 
 use LaDanse\SiteBundle\Model\ErrorModel;
 
@@ -23,7 +24,7 @@ class EditClaimController extends LaDanseController
     /**
      * @Route("/{claimId}/edit", name="editClaim")
      */
-    public function editAction($claimId)
+    public function editAction(Request $request, $claimId)
     {
     	$authContext = $this->getAuthenticationService()->getCurrentContext();
 
@@ -33,5 +34,65 @@ class EditClaimController extends LaDanseController
 
             return $this->redirect($this->generateUrl('welcomeIndex'));
         }
-    } 
+
+        $accountId = $authContext->getAccount()->getId();
+
+        $claimModel = $this->getGuildCharacterService()->getClaim($claimId);
+
+        $formModel = new EditClaimFormModel($claimModel);
+
+        $form = $this->createForm(new EditClaimFormType($this->getContainerInjector()), $formModel, 
+            array('attr' => array('class' => 'form-horizontal', 'novalidate' => '')));
+
+        if ($request->getMethod() == 'POST')
+        {
+           $form->handleRequest($request);
+
+            $errors = new ErrorModel();
+
+            if ($form->isValid() && $formModel->isValid($errors))
+            {
+                $this->updateClaim($claimId, $formModel);
+
+                $this->addToast('Character claim updated');
+
+                return $this->redirect($this->generateUrl('viewClaims'));
+            }
+            else
+            {
+                return $this->render('LaDanseSiteBundle:claims:editClaim.html.twig',
+                        array('claim' => $claimModel, 'form' => $form->createView(), 'errors' => $errors));
+            }
+        }
+        else
+        {
+            return $this->render('LaDanseSiteBundle:claims:editClaim.html.twig',
+                        array('claim' => $claimModel, 'form' => $form->createView()));
+        }   
+    }
+
+    public function updateClaim($claimId, $formModel)
+    {
+        $tank   = false;
+        $healer = false;
+        $dps    = false;
+
+        foreach($formModel->getRoles() as $role)
+        {
+            switch($role)
+            {
+                case Role::TANK:
+                    $tank = true;
+                    break;
+                case Role::HEALER:
+                    $healer = true;
+                    break;
+                case Role::DPS:
+                    $dps = true;
+                    break;       
+            }
+        }
+
+        $this->getGuildCharacterService()->updateClaim($claimId, $tank, $healer, $dps);    
+    }    
 }
