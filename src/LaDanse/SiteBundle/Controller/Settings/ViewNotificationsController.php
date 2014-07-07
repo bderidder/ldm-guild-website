61d163f1fbb1ae889a690738f10d80ce2ba8b5a2
Bavo De Ridder <bavo@coderspotting.org>
2014-07-07 22:06:26 +0200

    Initial revision

---
+++ src/LaDanse/SiteBundle/Controller/Settings/ViewNotificationsController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace LaDanse\SiteBundle\Controller\Settings;
+
+use Symfony\Component\HttpFoundation\Request;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+
+use LaDanse\CommonBundle\Helper\LaDanseController;
+
+use LaDanse\SiteBundle\Form\Model\SettingsFormModel;
+use LaDanse\SiteBundle\Form\Type\SettingsFormType;
+
+class ViewNotificationsController extends LaDanseController
+{
+	/**
+     * @Route("/notifications", name="viewNotifications")
+     * @Template("LaDanseSiteBundle:settings:viewNotifications.html.twig")
+     */
+    public function indexAction(Request $request)
+    {
+    	$authContext = $this->getAuthenticationService()->getCurrentContext();
+
+    	if (!$authContext->isAuthenticated())
+    	{
+            $this->getLogger()->warn(__CLASS__ . ' the user was not authenticated in indexAction');
+
+    		return $this->redirect($this->generateUrl('welcomeIndex'));
+    	}
+    }
+}
---
+++ src/LaDanse/SiteBundle/Controller/Settings/ViewProfileController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace LaDanse\SiteBundle\Controller\Settings;
+
+use Symfony\Component\HttpFoundation\Request;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+
+use LaDanse\CommonBundle\Helper\LaDanseController;
+
+use LaDanse\SiteBundle\Form\Model\SettingsFormModel;
+use LaDanse\SiteBundle\Form\Type\SettingsFormType;
+
+class ViewProfileController extends LaDanseController
+{
+	/**
+     * @Route("/profile", name="viewProfile")
+     * @Template("LaDanseSiteBundle:settings:viewProfile.html.twig")
+     */
+    public function indexAction(Request $request)
+    {
+    	$authContext = $this->getAuthenticationService()->getCurrentContext();
+
+    	if (!$authContext->isAuthenticated())
+    	{
+            $this->getLogger()->warn(__CLASS__ . ' the user was not authenticated in indexAction');
+
+    		return $this->redirect($this->generateUrl('welcomeIndex'));
+    	}
+    }
+}
