c5bef526fd67accfd93305a17c49bd3689428224
Bavo De Ridder <bderidder@gmail.com>
2014-11-15 08:39:19 +0100

    Initial revision

--- src/LaDanse/ServicesBundle/Service/AccountService.php
+++ src/LaDanse/ServicesBundle/Service/AccountService.php
@@ -8,9 +8,9 @@ use LaDanse\CommonBundle\Helper\LaDanseService;
 
 use LaDanse\DomainBundle\Entity\Account;
 
-class SettingsService extends LaDanseService
+class AccountService extends LaDanseService
 {
-    const SERVICE_NAME = 'LaDanse.SettingsService';
+    const SERVICE_NAME = 'LaDanse.AccountService';
 
 	public function __construct(ContainerInterface $container)
 	{
@@ -82,96 +82,5 @@ class SettingsService extends LaDanseService
         $result = $query->getResult();
 
         return !(count($result) == 0);
-    }
-
-    /*
-     * Return for a specific account all known settings
-     */
-    public function getSettingsForAccount($accountId, $settingNamePrefix = '')
-    {
-        $em = $this->getDoctrine()->getManager();
-
-        /* @var $query \Doctrine\ORM\Query */
-        $query = $em->createQuery(
-            $this->createSQLFromTemplate('LaDanseDomainBundle:settings:selectSettingsForAccount.sql.twig'));
-        $query->setParameter('accountId', $accountId);
-        $query->setParameter('namePattern', $settingNamePrefix . '%');
-        
-        $settings = $query->getResult();
-
-        $settingModels = array();
-
-        foreach($settings as $setting)
-        {
-            $settingModels[] = $this->settingToDto($setting);
-        }
-
-        return $settingModels;
-    }
-
-    /*
-     * Return for a specific setting (prefix), the value (if it exists) for each account
-     */
-    public function getSettingsForAllAccounts($settingNamePrefix)
-    {
-        $em = $this->getDoctrine()->getManager();
-
-        /* @var $query \Doctrine\ORM\Query */
-        $query = $em->createQuery(
-            $this->createSQLFromTemplate('LaDanseDomainBundle:settings:selectSettingsForAllAccounts.sql.twig'));
-        $query->setParameter('namePattern', $settingNamePrefix . '%');
-        
-        $settings = $query->getResult();
-
-        $settingModels = array();
-
-        foreach($settings as $setting)
-        {
-            $settingModels[] = $this->settingToDto($setting);
-        }
-
-        return $settingModels;
-    }
-
-    /*
-     * Update all settings passed for an account, settings not passed as
-     * a parameter are left untouched.
-     * Settings that didn't exist yet are created on the fly.
-     */
-    public function updateSettingsForAccount($accountId, $settings)
-    {
-    }
-
-    /*
-     * Remove settings for the given account
-     */
-    public function removeSettingsForAccount($accountId, $settingNames = '')
-    {
-    }
-
-    protected function doesSettingExist($settings, $settingName)
-    {
-        foreach($settings as $setting)
-        {
-            if ($setting->name == $settingName)
-            {
-                return true;
-            }
-        }
-
-        return false;
-    }
-
-    /**
-     * @param $setting \LaDanse\DomainBundle\Entity\Setting
-     * @return object
-     */
-    protected function settingToDto($setting)
-    {
-        return (object) array(
-            'name'      => $setting->getName(),
-            'value'     => $setting->getValue(),
-            'accountId' => $setting->getAccount()->getId()
-        );
     }
 }
