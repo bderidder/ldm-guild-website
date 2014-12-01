8ce02180acc76e7eacafc11511d8ac5dd9d0e9f5
Bavo De Ridder <bderidder@gmail.com>
2014-12-01 18:27:24 +0100

    Added methods for logging

--- src/LaDanse/ServicesBundle/Command/CommandExecutionContext.php
+++ src/LaDanse/ServicesBundle/Command/CommandExecutionContext.php
@@ -17,11 +17,62 @@ use \Symfony\Component\Console\Output\OutputInterface;
  */
 class CommandExecutionContext
 {
+    /** @var $input InputInterface */
     private $input;
+    /** @var $output OutputInterface */
     private $output;
 
-    function _construct(InputInterface $input, OutputInterface $output)
-    {
+    private $isVerbose;
+    private $isDiag;
 
+    /**
+     * @param InputInterface $input
+     * @param OutputInterface $output
+     */
+    public function __construct(InputInterface $input, OutputInterface $output, $isVerbose, $isDiag)
+    {
+        $this->input = $input;
+        $this->output = $output;
+
+        $this->isVerbose = $isVerbose;
+        $this->isDiag = $isDiag;
+    }
+
+    /**
+     * @return InputInterface
+     */
+    public function getInput()
+    {
+        return $this->input;
+    }
+
+    /**
+     * @return OutputInterface
+     */
+    public function getOutput()
+    {
+        return $this->output;
+    }
+
+    /**
+     * @param $text
+     */
+    public function debug($text)
+    {
+        if ($this->isDiag)
+        {
+            $this->output->writeln($text);
+        }
+    }
+
+    /**
+     * @param $text
+     */
+    public function info($text)
+    {
+        if ($this->isVerbose or $this->isDiag)
+        {
+            $this->output->writeln($text);
+        }
     }
 } 
