d0569e890e86fc8dd4cad98f6c25a08f33303d20
Bavo De Ridder <bderidder@gmail.com>
2014-12-03 08:43:32 +0100

    Initial revision

---
+++ src/LaDanse/ForumBundle/Controller/TopicsResource.php
@@ -0,0 +1,241 @@
+<?php
+/**
+ * @license  http://opensource.org/licenses/gpl-license.php GNU Public License
+ * @link     https://github.com/bderidder/ldm-guild-website
+ */
+
+namespace LaDanse\ForumBundle\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\JsonResponse;
+
+use LaDanse\CommonBundle\Helper\LaDanseController;
+
+use LaDanse\ForumBundle\Entity\Post;
+
+use LaDanse\ForumBundle\Service\TopicDoesNotExistException;
+
+/**
+ * @Route("/topics/{topicId}/posts")
+ */
+class PostsResource extends LaDanseController
+{
+    /**
+     * @param Request $request
+     * @param string $topicId
+     *
+     * @return Response
+     *
+     * @Route("/", name="getPosts")
+     * @Method({"GET"})
+     */
+    public function getPostsAction(Request $request, $topicId)
+    {
+        try
+        {
+            $posts = $this->getForumService()->getAllPosts($topicId);
+        }
+        catch (TopicDoesNotExistException $e)
+        {
+            return ResourceHelper::createErrorResponse(
+                $request,
+                Response::HTTP_NOT_FOUND,
+                $e->getMessage(),
+                array("Allow" => "GET")
+            );
+        }
+
+        usort(
+            $posts,
+            function ($a, $b) {
+                /** @var $a \LaDanse\ForumBundle\Entity\Post */
+                /** @var $b \LaDanse\ForumBundle\Entity\Post */
+
+                return $a->getPostDate() > $b->getPostDate();
+            }
+        );
+
+        $jsonArray = array();
+
+        foreach ($posts as $post)
+        {
+            $jsonArray[] = $this->postToJson($post);
+        }
+
+        $jsonObject = (object)array(
+            "posts" => $jsonArray,
+            "links" => (object)array(
+                "self" => $this->generateUrl('getPosts', array('topicId' => $topicId), true)
+            )
+        );
+
+        return new JsonResponse($jsonObject);
+    }
+
+    /**
+     * @param string $topicId
+     *
+     * @Route("/create", name="createPost")
+     * @Method({"GET"})
+     */
+    public function createTopicAction($topicId)
+    {
+        $authContext = $this->getAuthenticationService()->getCurrentContext();
+
+        $this->getForumService()->createPost($topicId, $authContext->getAccount(), 'This is a message');
+    }
+
+    /**
+     * @param Request $request
+     * @param string $topicId
+     *
+     * @return Response
+     *
+     * @Route("/{postId}", name="getPost")
+     * @Method({"GET"})
+     */
+    public function getTopicAction(Request $request, $topicId)
+    {
+        return ResourceHelper::createErrorResponse(
+            $request,
+            Response::HTTP_NOT_FOUND,
+            "Resource not found (" . $topicId . ")",
+            array("Allow" => "GET")
+        );
+    }
+
+    /**
+     * @param Request $request
+     * @param string $topicId
+     *
+     * @return Response
+     *
+     * @Route("/", name="createPost")
+     * @Method({"POST", "PUT"})
+     */
+    public function createPostAction(Request $request, $topicId)
+    {
+        $authContext = $this->getAuthenticationService()->getCurrentContext();
+
+        $jsonData = $request->getContent(false);
+
+        $logger = $this->get('logger');
+        $logger->info('JSON DATA ' . $jsonData);
+
+        $jsonObject = json_decode($jsonData);
+
+        $this->getForumService()->createPost($topicId, $authContext->getAccount(), $jsonObject->message);
+
+        $jsonObject = (object)array(
+            "posts" => "test"
+        );
+
+        return new JsonResponse($jsonObject);
+    }
+
+    /**
+     * @param Request $request
+     * @param string $postId
+     *
+     * @return Response
+     *
+     * @Route("/{postId}", name="updatePost")
+     * @Method({"POST", "PUT"})
+     */
+    public function updatePostAction(Request $request, $postId)
+    {
+        $authContext = $this->getAuthenticationService()->getCurrentContext();
+
+        $post = null;
+
+        try
+        {
+            $post = $this->getForumService()->getPost($postId);
+        }
+        catch (TopicDoesNotExistException $e)
+        {
+            return ResourceHelper::createErrorResponse(
+                $request,
+                Response::HTTP_NOT_FOUND,
+                $e->getMessage(),
+                array("Allow" => "GET")
+            );
+        }
+
+        if (!($post->getPoster()->getId() == $authContext->getAccount()->getId()))
+        {
+            return ResourceHelper::createErrorResponse(
+                $request,
+                Response::HTTP_FORBIDDEN,
+                'Not allowed',
+                array("Allow" => "GET")
+            );
+        }
+
+        $jsonData = $request->getContent(false);
+
+        $jsonObject = json_decode($jsonData);
+
+        $this->getForumService()->updatePost($postId, $jsonObject->message);
+
+        $jsonObject = (object)array(
+            "posts" => "test"
+        );
+
+        return new JsonResponse($jsonObject);
+    }
+
+    /**
+     * @param Request $request
+     * @param string $topicId
+     * @param string $commentId
+     *
+     * @return Response
+     *
+     * @Route("/{commentId}", name="otherPost")
+     * @Method({"POST", "PUT", "DELETE", "OPTIONS"})
+     */
+    public function otherPostAction(Request $request, $topicId, $commentId)
+    {
+        $this->getLogger()->warning(
+            'POST/PUT/DELETE/OPTIONS for Comment resource with ' . $topicId . ' and ' . $commentId
+        );
+
+        return ResourceHelper::createErrorResponse(
+            $request,
+            Response::HTTP_NOT_FOUND,
+            "Resource not found",
+            array("Allow" => "GET")
+        );
+    }
+
+    /**
+     * @param Post $post
+     *
+     * @return object
+     */
+    private function postToJson(Post $post)
+    {
+        return (object)array(
+            "postId" => $post->getId(),
+            "posterId" => $post->getPoster()->getId(),
+            "poster" => $post->getPoster()->getDisplayName(),
+            "message" => $post->getMessage(),
+            "postDate" => $post->getPostDate()->format(\DateTime::ISO8601),
+            "links" => (object)array(
+                "self" => $this->generateUrl(
+                    'getPost',
+                    array(
+                        'topicId' => $post->getTopic()->getId(),
+                        'postId' => $post->getId()
+                    ),
+                    true
+                ),
+            )
+        );
+    }
+}
