0ee7fc910fda03210278427e00e0ae0f92918f08
Bavo De Ridder <bderidder@gmail.com>
2014-12-12 11:42:15 +0100

    Non-authenticated users should be redirected

--- src/LaDanse/SiteBundle/Controller/Forum/UnderConstructionController.php
+++ src/LaDanse/SiteBundle/Controller/Forum/UnderConstructionController.php
@@ -7,11 +7,20 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 
 class UnderConstructionController extends LaDanseController
 {
-	/**
+    /**
      * @Route("/", name="forumUnderConstruction")
      */
     public function indexAction()
     {
+        $authContext = $this->getAuthenticationService()->getCurrentContext();
+
+        if (!$authContext->isAuthenticated())
+        {
+            $this->getLogger()->warning(__CLASS__ . ' the user was not authenticated in forumUnderConstruction');
+
+            return $this->redirect($this->generateUrl('welcomeIndex'));
+        }
+
         return $this->render("LaDanseSiteBundle:forum:underConstruction.html.twig");
     }
 }
