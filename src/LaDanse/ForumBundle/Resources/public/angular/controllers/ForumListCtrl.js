36437a8ca5e0a7692d6d071443ffbfc219d2eb21
Bavo De Ridder <bderidder@gmail.com>
2014-12-18 22:35:59 +0100

    Controller now keeps a list of forums but doesn't display them itself

--- src/LaDanse/ForumBundle/Resources/public/angular/controllers/ForumListCtrl.js
+++ src/LaDanse/ForumBundle/Resources/public/angular/controllers/ForumListCtrl.js
@@ -1,18 +1,33 @@
 forumControllers.controller('ForumListCtrl',
     function ($scope, $routeParams, $rootScope, $http)
     {
-        $scope.forums = [];
-        $scope.isCollapsed = true;
-
         $scope.initForumListCtrl = function()
         {
-            $scope.refreshForums();
+            $scope.refreshTopics();
         };
 
-        $scope.refreshForums = function()
+        $scope.getForum = function(forumId)
+        {
+            var arrayLength = $scope.forums.length;
+            for (var i = 0; i < arrayLength; i++)
+            {
+                var forum = $scope.forums[i];
+
+                if (forum.forumId == forumId)
+                {
+                    return forum;
+                }
+            }
+
+            return null;
+        }
+
+        $scope.refreshTopics = function()
         {
             $http.get('../services/forum/forums').success(function(data) {
                 $scope.forums = data.forums;
+                $scope.topics = data.topics;
+                $scope.isForumLoaded = true;
             });
         }
     }
