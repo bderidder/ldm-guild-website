2ebd01808561c00527be4fe3cfe5fc784a13a65b
Bavo De Ridder <bderidder@gmail.com>
2014-12-21 20:57:23 +0100

    Added animation directive

--- src/LaDanse/ForumBundle/Resources/public/angular/app/ForumApp.js
+++ src/LaDanse/ForumBundle/Resources/public/angular/app/ForumApp.js
@@ -1,5 +1,5 @@
 var forumApp = angular.module('ForumApp',
-    ['ngRoute', 'ngResource', 'ui.bootstrap', 'ngSanitize', 'textAngular', 'forumControllers']
+    ['ngRoute', 'ngResource', 'ui.bootstrap', 'ngSanitize', 'textAngular', 'ngAnimate', 'forumControllers']
 );
 
 var forumControllers = angular.module('forumControllers', ['ngRoute', 'ngResource']);
@@ -20,6 +20,31 @@ forumApp.directive('forumItemTemplate', function() {
     return {
         templateUrl: '/bundles/ladanseforum/angular/partials/ForumItemView.html',
     };
+});
+
+forumApp.animation('.slide-animation', function()
+{
+    return {
+        addClass: function (element, className, done) {
+            if (className == 'ng-hide') {
+                TweenMax.to(element, 0.5, {left: -element.parent().width(), onComplete: done});
+            }
+            else {
+                done();
+            }
+        },
+        removeClass: function (element, className, done) {
+            if (className == 'ng-hide') {
+                element.removeClass('ng-hide');
+
+                TweenMax.set(element, {left: element.parent().width()});
+                TweenMax.to(element, 0.5, {left: 0, onComplete: done});
+            }
+            else {
+                done();
+            }
+        }
+    }
 });
 
 forumApp.config(['$routeProvider',
