2f81d3487479e6b786df0ebc1ad2281587e07cf1
Bavo De Ridder <bderidder@gmail.com>
2015-01-11 15:48:27 +0100

    Added debugging

--- src/LaDanse/ForumBundle/Resources/public/angular/app/ForumApp.js
+++ src/LaDanse/ForumBundle/Resources/public/angular/app/ForumApp.js
@@ -34,22 +34,32 @@ forumApp.directive('forumItemTemplate', function() {
 forumApp.animation('.slide-animation', function()
 {
     return {
-        addClass: function (element, className, done) {
-            if (className == 'ng-hide') {
+        addClass: function (element, className, done)
+        {
+            if (className == 'ng-hide')
+            {
+                console.log('move element out to the left');
                 TweenMax.to(element, 0.5, {left: -element.parent().width(), onComplete: done});
             }
-            else {
+            else
+            {
                 done();
             }
         },
-        removeClass: function (element, className, done) {
-            if (className == 'ng-hide') {
-                element.removeClass('ng-hide');
+        removeClass: function (element, className, done)
+        {
+            if (className == 'ng-hide')
+            {
+                //element.removeClass('ng-hide');
 
+                console.log('set element ready on the right');
                 TweenMax.set(element, {left: element.parent().width()});
+
+                console.log('move element in from the right');
                 TweenMax.to(element, 0.5, {left: 0, onComplete: done});
             }
-            else {
+            else
+            {
                 done();
             }
         }
