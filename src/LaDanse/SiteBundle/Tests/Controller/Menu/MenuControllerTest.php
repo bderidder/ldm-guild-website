778542bc233d1652304782190d3a59150d4c151d
Bavo De Ridder <bderidder@gmail.com>
2015-03-21 23:20:51 +0100

    Uses MemberTestBase now

--- src/LaDanse/SiteBundle/Tests/Controller/Menu/MenuControllerTest.php
+++ src/LaDanse/SiteBundle/Tests/Controller/Menu/MenuControllerTest.php
@@ -2,6 +2,7 @@
 
 namespace LaDanse\SiteBundle\Tests\Controller\Menu;
 
+use LaDanse\SiteBundle\Tests\Controller\MemberTestBase;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
@@ -17,35 +18,9 @@ use LaDanse\SiteBundle\Tests\Controller\AccountConst;
  * @license  http://opensource.org/licenses/gpl-license.php GNU Public License
  * @link     https://github.com/bderidder/ldm-guild-website
  */
-class MenuControllerTest extends WebTestCase
+class MenuControllerTest extends MemberTestBase
 {
     const CONTROLLER_URL = "/menu/";
-
-    /**
-     * Test if the welcome page shows the "more information" button
-     *
-     * @return void
-     */
-    public function testLoginRedirect()
-    {
-        $client = static::createClient();
-
-        $client->request('GET', MenuControllerTest::CONTROLLER_URL);
-
-        $this->assertTrue($client->getResponse() instanceof RedirectResponse);
-
-        $this->assertRegExp('/\/$/', $client->getResponse()->headers->get('location'));
-
-        $crawler = $client->followRedirect();
-
-        $this->assertTrue(
-            $crawler->filter('html:contains("login or register")')->count() > 0
-        );
-
-        $this->assertTrue(
-            $crawler->filter('html:contains("member section")')->count() == 0
-        );
-    }
 
     /**
      * Test if requesting / with a logged-in user gives menu
@@ -73,35 +48,8 @@ class MenuControllerTest extends WebTestCase
         );
     }
 
-    /**
-     * Test if the welcome page contains the username in Javascript
-     *
-     * @return void
-     */
-    public function testUsernameInJS()
+    protected function getUrl()
     {
-        $client = static::createClient(
-            array(),
-            array(
-                'PHP_AUTH_USER' => AccountConst::USER1_USERNAME,
-                'PHP_AUTH_PW'   => AccountConst::USER1_PASSWORD,
-            )
-        );
-
-        $client->followRedirects();
-
-        $crawler = $client->request('GET', MenuControllerTest::CONTROLLER_URL);
-
-        $this->assertTrue(
-            $crawler->filter(
-                'html:contains("username: \'' . AccountConst::USER1_USERNAME . '\'")'
-            )->count() == 1
-        );
-
-        $this->assertTrue(
-            $crawler->filter(
-                'html:contains("displayName: \'' . AccountConst::USER1_DISPLAY . '\'")'
-            )->count() == 1
-        );
+        return MenuControllerTest::CONTROLLER_URL;
     }
 }
