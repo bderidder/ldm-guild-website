7ec30f85c35e9ff0d74b340c1aafb17d79171cee
Bavo De Ridder <bderidder@gmail.com>
2015-05-23 12:28:40 +0200

    Initial version

---
+++ docs/sql/RaidForums.sql
@@ -0,0 +1,14 @@
+INSERT INTO Forum (forumId, name, description) 
+	VALUES ('3908cb8b86657cdaf71b020ed0115z21', 'The Walled City', 'Kargath, Butcher and Brackenspore');
+INSERT INTO Forum (forumId, name, description) 
+	VALUES ('3908cb8b86657cdaf71b020ed0115z22', 'Arcane Sanctum', 'Tectus, Twin Ogron and Ko\'ragh');
+INSERT INTO Forum (forumId, name, description) 
+	VALUES ('3908cb8b86657cdaf71b020ed0115z23', 'Imperator', 'Imperator Mor\'gok');
+INSERT INTO Forum (forumId, name, description) 
+	VALUES ('3908cb8b86657cdaf71b020ed0115z24', 'Slagworks', 'Oregorger, Gruul and Blast Furnace');
+INSERT INTO Forum (forumId, name, description) 
+	VALUES ('3908cb8b86657cdaf71b020ed0115z25', 'Black Forge', 'Hans\'gar and Franzok, Flamebender Ka\'graz and Kromog');
+INSERT INTO Forum (forumId, name, description) 
+	VALUES ('3908cb8b86657cdaf71b020ed0115z26', 'Iron Assembly', 'Beastlord Darmac, Operator Thogar and Iron Maidens');
+INSERT INTO Forum (forumId, name, description) 
+	VALUES ('3908cb8b86657cdaf71b020ed0115z27', 'Blackhand', 'Blackhand');
