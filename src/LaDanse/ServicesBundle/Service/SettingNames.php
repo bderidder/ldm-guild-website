4bdaa8e0032329159043a6d10e33ba4a5f51256c
Bavo De Ridder <bavo@coderspotting.org>
2015-07-19 17:42:05 +0200

    Added new setting names

--- src/LaDanse/ServicesBundle/Service/SettingNames.php
+++ src/LaDanse/ServicesBundle/Service/SettingNames.php
@@ -5,5 +5,6 @@ namespace LaDanse\ServicesBundle\Service;
 class SettingNames
 {
     const NOTIFICATIONS_FORUMS_TOPIC_CREATED = "notifications.forums.topicCreated";
-    const NOTIFICATIONS_FORUMS_POST_CREATED  = "notifications.forums.postCreated";
+    const NOTIFICATIONS_FORUMS_POST_REPLY    = "notifications.forums.replyCreated";
+    const NOTIFICATIONS_FORUMS_ALL_POSTS     = "notifications.forums.allPosts";
 }
