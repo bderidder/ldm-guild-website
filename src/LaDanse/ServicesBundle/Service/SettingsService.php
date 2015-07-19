f57409545fcb0d5b9afe998093e4cc5a7a9c013b
Bavo De Ridder <bavo@coderspotting.org>
2015-07-19 17:42:26 +0200

    Layouting changes

--- src/LaDanse/ServicesBundle/Service/SettingsService.php
+++ src/LaDanse/ServicesBundle/Service/SettingsService.php
@@ -142,7 +142,7 @@ class SettingsService extends LaDanseService
         return (object) array(
             'name'      => $setting->getName(),
             'value'     => $setting->getValue(),
-            'account' => $setting->getAccount()
+            'account'   => $setting->getAccount()
         );
     }
 }
