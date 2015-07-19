bd5bd6bb68321abf5e8db802d0a7e649ee7ddb70
Bavo De Ridder <bavo@coderspotting.org>
2015-07-19 15:31:46 +0200

    Added setting name for a post in the forums

--- src/LaDanse/ServicesBundle/Service/SettingNames.php
+++ src/LaDanse/ServicesBundle/Service/SettingNames.php
@@ -5,4 +5,5 @@ namespace LaDanse\ServicesBundle\Service;
 class SettingNames
 {
     const NOTIFICATIONS_FORUMS_TOPIC_CREATED = "notifications.forums.topicCreated";
+    const NOTIFICATIONS_FORUMS_POST_CREATED  = "notifications.forums.postCreated";
 }
