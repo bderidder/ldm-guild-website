2d266861f23e1fde4b4175599713ac6a7b2d79f2
Bavo De Ridder <bavo@coderspotting.org>
2015-07-19 08:59:54 +0200

    Initial revision

---
+++ src/LaDanse/ServicesBundle/Notification/Notificators/TestNotificator.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace LaDanse\ServicesBundle\Notification\Notificators;
+
+use JMS\DiExtraBundle\Annotation as DI;
+use LaDanse\DomainBundle\Entity\NotificationQueueItem;
+use LaDanse\ServicesBundle\Notification\AbstractNotificator;
+use LaDanse\ServicesBundle\Notification\NotificationContext;
+use LaDanse\ServicesBundle\Notification\NotificationTemplates;
+
+/**
+ * @DI\Service(TestNotificator::SERVICE_NAME, public=true)
+ */
+class TestNotificator extends AbstractNotificator
+{
+    const SERVICE_NAME = 'LaDanse.TestNotificator';
+
+    public function processNotificationItem(
+        NotificationQueueItem $queueItem,
+        NotificationContext $context)
+    {
+        $toEmail = $this->container->getParameter('admin_email');
+
+        $this->logger->debug(
+            sprintf("%s - sending test email to %s",
+                __CLASS__,
+                $toEmail
+            )
+        );
+
+        $context->addMail(
+            $toEmail,
+            "Test Notification - " . $queueItem->getActivityType(),
+            $queueItem->getData(),
+            NotificationTemplates::TEST,
+            100
+        );
+    }
+}
