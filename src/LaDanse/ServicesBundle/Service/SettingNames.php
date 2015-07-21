88dac53ce399feb9c5b7c8ed0c465398fd8427b3
Bavo De Ridder <bavo@coderspotting.org>
2015-07-21 23:13:00 +0200

    Added setting for notifications when an event is created

--- src/LaDanse/ServicesBundle/Service/SettingNames.php
+++ src/LaDanse/ServicesBundle/Service/SettingNames.php
@@ -7,4 +7,6 @@ class SettingNames
     const NOTIFICATIONS_FORUMS_TOPIC_CREATED = "notifications.forums.topicCreated";
     const NOTIFICATIONS_FORUMS_POST_REPLY    = "notifications.forums.replyCreated";
     const NOTIFICATIONS_FORUMS_ALL_POSTS     = "notifications.forums.allPosts";
+
+    const NOTIFICATIONS_EVENT_CREATED = "notifications.eventCreated";
 }
