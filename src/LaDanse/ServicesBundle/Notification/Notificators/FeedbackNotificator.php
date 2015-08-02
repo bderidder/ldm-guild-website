9c4c57bd94a975f10fb1c09c15575c006beaa5af
Bavo De Ridder <bavo@coderspotting.org>
2015-08-02 16:24:46 +0200

    Initial revision

---
+++ src/LaDanse/ServicesBundle/Notification/Notificators/FeedbackNotificator.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace LaDanse\ServicesBundle\Notification\Notificators;
+
+use JMS\DiExtraBundle\Annotation as DI;
+use LaDanse\DomainBundle\Entity\NotificationQueueItem;
+use LaDanse\ServicesBundle\Notification\AbstractNotificator;
+use LaDanse\ServicesBundle\Notification\NotificationContext;
+use LaDanse\ServicesBundle\Notification\NotificationTemplates;
+
+/**
+ * @DI\Service(FeedbackNotificator::SERVICE_NAME, public=true)
+ */
+class FeedbackNotificator extends AbstractNotificator
+{
+    const SERVICE_NAME = 'LaDanse.FeedbackNotificator';
+
+    public function processNotificationItem(
+        NotificationQueueItem $queueItem,
+        NotificationContext $context)
+    {
+        $toEmail = $this->container->getParameter('admin_email');
+
+        $this->logger->debug(
+            sprintf("%s - sending test email to %s",
+                __CLASS__,
+                $toEmail
+            )
+        );
+
+        $context->addMail(
+            $toEmail,
+            "Feedback Notification",
+            array(
+              'account'      => $queueItem->getActivityBy(),
+              'activityData' => $queueItem->getData()
+            ),
+            NotificationTemplates::FEEDBACK
+        );
+    }
+}
