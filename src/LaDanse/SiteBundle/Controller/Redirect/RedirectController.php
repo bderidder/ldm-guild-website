e30080d3cf1e85a8b7df1cb30220bd6191c1db2f
Bavo De Ridder <bavo@coderspotting.org>
2015-08-08 17:05:54 +0200

    Initial revision

---
+++ src/LaDanse/SiteBundle/Controller/Redirect/RedirectController.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace LaDanse\SiteBundle\Controller\Redirect;
+
+use LaDanse\CommonBundle\Helper\LaDanseController;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Component\EventDispatcher\EventDispatcherInterface;
+use Symfony\Component\HttpFoundation\Response;
+
+use LaDanse\ServicesBundle\Activity\ActivityEvent;
+use LaDanse\ServicesBundle\Activity\ActivityType;
+
+use JMS\DiExtraBundle\Annotation as DI;
+
+class RedirectController extends LaDanseController
+{
+    /**
+     * @var $logger \Monolog\Logger
+     * @DI\Inject("monolog.logger.ladanse")
+     */
+    private $logger;
+
+    /**
+     * @var $eventDispatcher EventDispatcherInterface
+     * @DI\Inject("event_dispatcher")
+     */
+    private $eventDispatcher;
+
+	/**
+     * @param string $url URL to redirect to
+     *
+     * @return Response
+     *
+     * @Route("/{url}", name="redirect", requirements={"url"=".+"})
+     */
+    public function indexAction($url)
+    {
+        if ($this->getAuthenticationService()->getCurrentContext()->isAuthenticated())
+        {
+            $account = $this->getAuthenticationService()->getCurrentContext()->getAccount();
+        }
+        else
+        {
+            $account = null;
+        }
+
+        $this->eventDispatcher->dispatch(
+            ActivityEvent::EVENT_NAME,
+            new ActivityEvent(
+                ActivityType::REDIRECT,
+                $account,
+                array(
+                    'url' => $url
+                )
+            )
+        );
+
+        return $this->redirect($url);
+    }
+}
