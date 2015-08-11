1fded8c8c4d4ae1a4b76c5f874fb19e87c7b356f
Bavo De Ridder <bavo@coderspotting.org>
2015-08-11 20:05:46 +0200

    Initial revision

---
+++ src/LaDanse/ServicesBundle/Notification/Notificators/DeleteEventNotificator.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace LaDanse\ServicesBundle\Notification\Notificators;
+
+use Doctrine\Bundle\DoctrineBundle\Registry;
+use JMS\DiExtraBundle\Annotation as DI;
+use LaDanse\DomainBundle\Entity\Event;
+use LaDanse\DomainBundle\Entity\NotificationQueueItem;
+use LaDanse\DomainBundle\Entity\SignUp;
+use LaDanse\ServicesBundle\Notification\AbstractNotificator;
+use LaDanse\ServicesBundle\Notification\ListFunctions;
+use LaDanse\ServicesBundle\Notification\NotificationContext;
+use LaDanse\ServicesBundle\Notification\NotificationTemplates;
+use LaDanse\ServicesBundle\Service\SettingNames;
+use LaDanse\ServicesBundle\Service\SettingsService;
+
+/**
+ * @DI\Service(DeleteEventNotificator::SERVICE_NAME, public=true)
+ */
+class DeleteEventNotificator extends AbstractNotificator
+{
+    const SERVICE_NAME = 'LaDanse.DeleteEventNotificator';
+
+    /**
+     * @var SettingsService $settingsService
+     * @DI\Inject(SettingsService::SERVICE_NAME)
+     */
+    public $settingsService;
+
+    public function processNotificationItem(
+        NotificationQueueItem $queueItem,
+        NotificationContext $context)
+    {
+        $settings = $this->settingsService->getSettingsForAllAccounts(SettingNames::NOTIFICATIONS_EVENT_CREATED);
+
+        /** @var mixed $setting */
+        foreach($settings as $setting)
+        {
+            if ($setting->account->getEmail() == $queueItem->getActivityBy()->getEmail())
+            {
+                // we don't send emails to the creator self
+                continue;
+            }
+
+            /** @var mixed $data */
+            $data = $queueItem->getData();
+
+            $this->logger->debug(
+                sprintf("%s - sending email to %s for new event",
+                    __CLASS__,
+                    $setting->account->getEmail()
+                )
+            );
+
+            $context->addMail(
+                $setting->account->getEmail(),
+                "Event Delete - " . $data->event->name,
+                array(
+                    'account'      => $queueItem->getActivityBy(),
+                    'activityData' => $data
+                ),
+                NotificationTemplates::EVENT_DELETE
+            );
+        }
+    }
+}
---
+++ src/LaDanse/ServicesBundle/Notification/Notificators/UpdateEventNotificator.php
@@ -0,0 +1,104 @@
+<?php
+
+namespace LaDanse\ServicesBundle\Notification\Notificators;
+
+use Doctrine\Bundle\DoctrineBundle\Registry;
+use JMS\DiExtraBundle\Annotation as DI;
+use LaDanse\DomainBundle\Entity\Event;
+use LaDanse\DomainBundle\Entity\NotificationQueueItem;
+use LaDanse\DomainBundle\Entity\SignUp;
+use LaDanse\ServicesBundle\Notification\AbstractNotificator;
+use LaDanse\ServicesBundle\Notification\ListFunctions;
+use LaDanse\ServicesBundle\Notification\NotificationContext;
+use LaDanse\ServicesBundle\Notification\NotificationTemplates;
+use LaDanse\ServicesBundle\Service\SettingNames;
+use LaDanse\ServicesBundle\Service\SettingsService;
+
+/**
+ * @DI\Service(UpdateEventNotificator::SERVICE_NAME, public=true)
+ */
+class UpdateEventNotificator extends AbstractNotificator
+{
+    const SERVICE_NAME = 'LaDanse.UpdateEventNotificator';
+
+    /**
+     * @var $logger \Monolog\Logger
+     * @DI\Inject("monolog.logger.ladanse")
+     */
+    public $logger;
+
+    /**
+     * @var SettingsService $settingsService
+     * @DI\Inject(SettingsService::SERVICE_NAME)
+     */
+    public $settingsService;
+
+    /**
+     * @var Registry $doctrine
+     * @DI\Inject("doctrine")
+     */
+    public $doctrine;
+
+    public function processNotificationItem(
+        NotificationQueueItem $queueItem,
+        NotificationContext $context)
+    {
+        $mailsSettings = $this->getEmailsHavingSetting(SettingNames::NOTIFICATIONS_EVENT_UPDATED);
+
+        /** @var mixed $data */
+        $data = $queueItem->getData();
+
+        $mailsSignUps = $this->getEmailsFromSignUps($data->oldEvent->eventId);
+
+        $sharedMails = ListFunctions::getIntersection($mailsSignUps, $mailsSettings);
+
+        /** @var mixed $setting */
+        foreach($sharedMails as $mail)
+        {
+            if ($mail == $queueItem->getActivityBy()->getEmail())
+            {
+                // we don't send emails to the creator self
+                continue;
+            }
+
+            /** @var mixed $data */
+            $data = $queueItem->getData();
+
+            $this->logger->debug(
+                sprintf("%s - sending email to %s for event",
+                    __CLASS__,
+                    $setting->account->getEmail()
+                )
+            );
+
+            $context->addMail(
+                $setting->account->getEmail(),
+                "Event Update - " . $data->event->name,
+                array(
+                    'account'      => $queueItem->getActivityBy(),
+                    'activityData' => $data
+                ),
+                NotificationTemplates::EVENT_UPDATE
+            );
+        }
+    }
+
+    private function getEmailsFromSignUps($eventId)
+    {
+        /* @var $repository \Doctrine\ORM\EntityRepository */
+        $repository = $this->doctrine->getRepository(Event::REPOSITORY);
+
+        /* @var $event \LaDanse\DomainBundle\Entity\Event */
+        $event = $repository->find($eventId);
+
+        $mails = array();
+
+        /** @var SignUp $signUp */
+        foreach($event->getSignUps() as $signUp)
+        {
+            $mails[] = $signUp->getAccount()->getEmail();
+        }
+
+        return ListFunctions::sortList($mails);
+    }
+}
