47ed9bee4479dcc2633ee1237c6ea40075ee8dad
Bavo De Ridder <bavo@coderspotting.org>
2015-11-30 21:34:40 +0100

    Initial revision

---
+++ src/LaDanse/ServicesBundle/Service/Event/Query/GetEventByIdQuery.php
@@ -0,0 +1,88 @@
+<?php
+
+namespace LaDanse\ServicesBundle\Service\Event\Query;
+
+use LaDanse\DomainBundle\Entity\Event;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+use Symfony\Component\EventDispatcher\EventDispatcherInterface;
+
+use LaDanse\CommonBundle\Helper\AbstractQuery;
+
+use JMS\DiExtraBundle\Annotation as DI;
+
+/**
+ * @DI\Service(GetEventByIdQuery::SERVICE_NAME, public=true, scope="prototype")
+ */
+class GetEventByIdQuery extends AbstractQuery
+{
+    const SERVICE_NAME = 'LaDanse.CreateEventCommand';
+
+    /**
+     * @var $logger \Monolog\Logger
+     * @DI\Inject("monolog.logger.ladanse")
+     */
+    public $logger;
+
+    /**
+     * @var $eventDispatcher EventDispatcherInterface
+     * @DI\Inject("event_dispatcher")
+     */
+    public $eventDispatcher;
+
+    /**
+     * @var $logger \Doctrine\Bundle\DoctrineBundle\Registry
+     * @DI\Inject("doctrine")
+     */
+    public $doctrine;
+
+    /** @var int $eventId */
+    private $eventId;
+
+    /**
+     * @param ContainerInterface $container
+     *
+     * @DI\InjectParams({
+     *     "container" = @DI\Inject("service_container")
+     * })
+     */
+    public function __construct(ContainerInterface $container)
+    {
+        parent::__construct($container);
+    }
+
+    /**
+     * @return int
+     */
+    public function getEventId()
+    {
+        return $this->eventId;
+    }
+
+    /**
+     * @param int $eventId
+     */
+    public function setEventId($eventId)
+    {
+        $this->eventId = $eventId;
+    }
+
+    protected function validateInput()
+    {
+    }
+
+    protected function runQuery()
+    {
+        /* @var $repository \Doctrine\ORM\EntityRepository */
+        $repository = $this->doctrine->getRepository(Event::REPOSITORY);
+
+        /* @var $event \LaDanse\DomainBundle\Entity\Event */
+        $event = $repository->find($this->getEventId());
+
+        if (is_null($event))
+        {
+            throw new EventDoesNotExistException('Event does not exist');
+        }
+
+        return $event;
+    }
+}
