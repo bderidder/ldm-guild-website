980de96a075964cbd596553741efd7dd9d0e9801
Bavo De Ridder <bavo@coderspotting.org>
2015-12-01 20:06:28 +0100

    Initial revision

---
+++ src/LaDanse/SiteBundle/Model/Calendar/CalendarMonthModel.php
@@ -0,0 +1,97 @@
+<?php
+
+namespace LaDanse\SiteBundle\Model\Calendar;
+
+
+class CalendarMonthModel
+{
+    /** @var \DateTime $startDate */
+    private $startDate;
+
+    /** @var \DateTime $endDate */
+    private $endDate;
+
+    /**
+     * Create a CalendarMonthModel that contains the given $date in the first or second week.
+     *
+     * It is guaranteed that the raid week the given $date is in, is fully shown in the calendar.
+     *
+     * @param \DateTime $date
+     */
+    public function __construct(\DateTime $date)
+    {
+        $this->init($date);
+    }
+
+    /**
+     * @return \DateTime
+     */
+    public function getStartDate()
+    {
+        return $this->startDate;
+    }
+
+    /**
+     * @return \DateTime
+     */
+    public function getEndDate()
+    {
+        return $this->endDate;
+    }
+
+    /**
+     * Shift the calendar one week to the past.
+     *
+     * @return CalendarMonthModel
+     */
+    public function shiftOneWeekBack()
+    {
+        $this->startDate->modify('-7 days');
+        $this->endDate->modify('-7 days');
+
+        return $this;
+    }
+
+    /**
+     * Verify if given $containedDate falls in the month represented by this model.
+     *
+     * @param \DateTime $containedDate
+     *
+     * @return true if the date falls in the represented month, false otherwise
+     */
+    public function containsDate(\DateTime $containedDate)
+    {
+        return (($this->startDate <= $containedDate) && ($containedDate <= $this->endDate));
+    }
+
+    private function init(\DateTime $date)
+    {
+        $day = date('w', $date->getTimestamp());
+
+        if ($day === 0)
+        {
+            // it's Sunday but in Europe we start our weeks on Monday
+            $day = 7;
+        }
+
+        // days are not from Monday (1) to Sunday (7)
+
+        if ($day < 3)
+        {
+            // it's monday or tuesday, we need to include the previous week in our
+            // calendar to show the current raid week completely
+
+            $day = $day + 6;
+        }
+        else
+        {
+            $day = $day - 1;
+        }
+
+        $this->startDate = clone $date;
+        $this->startDate->modify('-' . $day . ' days');
+
+        $this->endDate = clone $this->startDate;
+        $this->endDate->modify('+' . 27 . ' days');
+    }
+}
---
+++ src/LaDanse/SiteBundle/Tests/Model/Calendar/CalendarMonthModelTest.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace LaDanse\SiteBundle\Tests\Model\Calendar;
+
+use LaDanse\SiteBundle\Model\Calendar\CalendarMonthModel;
+
+class CalendarMonthModelTest extends \PHPUnit_Framework_TestCase
+{
+    public function testConstructorMonday()
+    {
+        /** @var CalendarMonthModel $calendarMonthModel */
+        $calendarMonthModel = new CalendarMonthModel(new \DateTime('2015-12-07'));
+
+        $this->assertEquals($calendarMonthModel->getStartDate(),  new \DateTime('2015-11-30'));
+        $this->assertEquals($calendarMonthModel->getEndDate(), new \DateTime('2015-12-27'));
+    }
+
+    public function testConstructorTuesday()
+    {
+        /** @var CalendarMonthModel $calendarMonthModel */
+        $calendarMonthModel = new CalendarMonthModel(new \DateTime('2015-12-08'));
+
+        $this->assertEquals($calendarMonthModel->getStartDate(),  new \DateTime('2015-11-30'));
+        $this->assertEquals($calendarMonthModel->getEndDate(), new \DateTime('2015-12-27'));
+    }
+
+    public function testConstructorWednesday()
+    {
+        /** @var CalendarMonthModel $calendarMonthModel */
+        $calendarMonthModel = new CalendarMonthModel(new \DateTime('2015-12-09'));
+
+        $this->assertEquals($calendarMonthModel->getStartDate(),  new \DateTime('2015-12-07'));
+        $this->assertEquals($calendarMonthModel->getEndDate(), new \DateTime('2016-01-03'));
+    }
+
+    public function testConstructorSunday()
+    {
+        /** @var CalendarMonthModel $calendarMonthModel */
+        $calendarMonthModel = new CalendarMonthModel(new \DateTime('2015-12-06'));
+
+        $this->assertEquals($calendarMonthModel->getStartDate(),  new \DateTime('2015-11-30'));
+        $this->assertEquals($calendarMonthModel->getEndDate(), new \DateTime('2015-12-27'));
+    }
+
+    public function testShiftOneWeekBack()
+    {
+        /** @var CalendarMonthModel $calendarMonthModel */
+        $calendarMonthModel = new CalendarMonthModel(new \DateTime('2015-12-13'));
+
+        $calendarMonthModel->shiftOneWeekBack();
+
+        $this->assertEquals($calendarMonthModel->getStartDate(),  new \DateTime('2015-11-30'));
+        $this->assertEquals($calendarMonthModel->getEndDate(), new \DateTime('2015-12-27'));
+    }
+}
