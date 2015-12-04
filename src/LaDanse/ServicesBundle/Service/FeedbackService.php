725db12601bfff4691317df62a55fd0c831a2cc9
Bavo De Ridder <bavo@coderspotting.org>
2015-12-04 22:22:20 +0100

    Initial revision

---
+++ src/LaDanse/ServicesBundle/Service/FeedbackService.php
@@ -0,0 +1,83 @@
+<?php
+/**
+ * @license  http://opensource.org/licenses/gpl-license.php GNU Public License
+ * @link     https://github.com/bderidder/ldm-guild-website
+ */
+
+namespace LaDanse\ServicesBundle\Service;
+
+use LaDanse\DomainBundle\Entity\Feedback;
+use LaDanse\ServicesBundle\Activity\ActivityEvent;
+use LaDanse\ServicesBundle\Activity\ActivityType;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+
+use LaDanse\CommonBundle\Helper\LaDanseService;
+use Symfony\Component\EventDispatcher\EventDispatcherInterface;
+
+use JMS\DiExtraBundle\Annotation as DI;
+
+/**
+ * Class FeedbackService
+ * @package LaDanse\ServicesBundle\Service
+ *
+ * @DI\Service(FeedbackService::SERVICE_NAME, public=true)
+ */
+class FeedbackService extends LaDanseService
+{
+    const SERVICE_NAME = 'LaDanse.FeedbackService';
+
+    /**
+     * @var $logger \Monolog\Logger
+     * @DI\Inject("monolog.logger.ladanse")
+     */
+    public $logger;
+
+    /**
+     * @var $eventDispatcher EventDispatcherInterface
+     * @DI\Inject("event_dispatcher")
+     */
+    public $eventDispatcher;
+
+    /**
+     * @var $logger \Doctrine\Bundle\DoctrineBundle\Registry
+     * @DI\Inject("doctrine")
+     */
+    public $doctrine;
+
+    /**
+     * @param ContainerInterface $container
+     *
+     * @DI\InjectParams({
+     *     "container" = @DI\Inject("service_container")
+     * })
+     */
+    public function __construct(ContainerInterface $container)
+    {
+        parent::__construct($container);
+    }
+
+    public function processFeedback($account, $description)
+    {
+        $em = $this->doctrine->getManager();
+
+        $feedback = new Feedback();
+
+        $feedback->setPostedBy($account);
+        $feedback->setPostedOn(new \DateTime());
+        $feedback->setFeedback($description);
+
+        $em->persist($feedback);
+
+        $em->flush();
+
+        $this->eventDispatcher->dispatch(
+            ActivityEvent::EVENT_NAME,
+            new ActivityEvent(
+                ActivityType::FEEDBACK_POST,
+                $this->getAuthenticationService()->getCurrentContext()->getAccount(),
+                array(
+                    'feedback' => $description
+                ))
+        );
+    }
+}
