7db85ac85b2d2541383eedd3b2d37141aaf1624c
Bavo De Ridder <bavo@coderspotting.org>
2015-12-09 21:14:40 +0100

    Moved $event retrieval down in the method to a more logical place

--- src/LaDanse/ServicesBundle/Service/Authorization/Policies/CreatorCanEditEventRule.php
+++ src/LaDanse/ServicesBundle/Service/Authorization/Policies/CreatorCanEditEventRule.php
@@ -3,6 +3,7 @@
 namespace LaDanse\ServicesBundle\Service\Authorization\Policies;
 
 use LaDanse\DomainBundle\Entity\Event;
+use LaDanse\ServicesBundle\Activity\ActivityType;
 use LaDanse\ServicesBundle\Service\Authorization\EvaluationCtx;
 use LaDanse\ServicesBundle\Service\Authorization\Rule;
 
@@ -10,17 +11,18 @@ class CreatorCanEditEventRule extends Rule
 {
     public function match(EvaluationCtx $evaluationCt)
     {
+        return $evaluationCt->getAction() == ActivityType::EVENT_EDIT;
     }
 
     public function evaluate(EvaluationCtx $evaluationCtx)
     {
-        /** @var Event $event */
-        $event = $evaluationCtx->getResourceValue();
-
         if ($evaluationCtx->getSubject()->isAnonymous())
         {
             return false;
         }
+
+        /** @var Event $event */
+        $event = $evaluationCtx->getResourceValue();
 
         $account = $evaluationCtx->getSubject()->getAccount();
 
