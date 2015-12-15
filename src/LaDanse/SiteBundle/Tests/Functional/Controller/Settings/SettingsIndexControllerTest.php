19fcd9b8abeb88903bd1adfb012e87b8568ea90c
Bavo De Ridder <bavo@coderspotting.org>
2015-12-15 21:54:33 +0100

    Initial revision

---
+++ src/LaDanse/SiteBundle/Tests/Functional/Controller/Settings/SettingsIndexControllerTest.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace LaDanse\SiteBundle\Tests\Functional\Controller\Settings;
+
+use LaDanse\DomainBundle\Entity\Account;
+use Liip\FunctionalTestBundle\Test\WebTestCase;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+
+class SettingsIndexControllerTest extends WebTestCase
+{
+    /**
+     * @return void
+     */
+    public function testRedirectToProfileSettings()
+    {
+        $fixtures = $this->loadFixtureFiles(array(
+            '@LaDanseSiteBundle/Tests/Fixtures/account.yml'
+        ));
+
+        /** @var Account $account */
+        $account = $fixtures['mainAccount'];
+
+        $client = static::makeClient(
+            array(),
+            array(
+                'PHP_AUTH_USER' => $account->getUsername(),
+                'PHP_AUTH_PW'   => 'test',
+            )
+        );
+
+        $client->followRedirects(false);
+
+        $client->request('GET', '/settings/');
+
+        $this->assertTrue(
+            $client->getResponse() instanceof RedirectResponse,
+            'Response was not a redirect response'
+        );
+
+        $this->assertRegExp('/\/settings\/profile$/', $client->getResponse()->headers->get('location'));
+    }
+}
