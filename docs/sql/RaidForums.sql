fb162e83078e3f48c3456bef6164438908e8cbac
Bavo De Ridder <bavo@coderspotting.org>
2016-04-07 21:24:03 +0200

    Reformatted so every SQL statement is on a single line

--- docs/sql/RaidForums.sql
+++ docs/sql/RaidForums.sql
@@ -1,24 +1,12 @@
-INSERT INTO Forum (forumId, name, description) 
-	VALUES ('3908cb8b86657cdaf71b020ed0115z21', 'The Walled City', 'Kargath, Butcher and Brackenspore');
-INSERT INTO Forum (forumId, name, description) 
-	VALUES ('3908cb8b86657cdaf71b020ed0115z22', 'Arcane Sanctum', 'Tectus, Twin Ogron and Ko\'ragh');
-INSERT INTO Forum (forumId, name, description) 
-	VALUES ('3908cb8b86657cdaf71b020ed0115z23', 'Imperator', 'Imperator Mor\'gok');
-INSERT INTO Forum (forumId, name, description) 
-	VALUES ('3908cb8b86657cdaf71b020ed0115z24', 'Slagworks', 'Oregorger, Gruul and Blast Furnace');
-INSERT INTO Forum (forumId, name, description) 
-	VALUES ('3908cb8b86657cdaf71b020ed0115z25', 'Black Forge', 'Hans and Franz, Flamebender Ka\'graz and Kromog');
-INSERT INTO Forum (forumId, name, description) 
-	VALUES ('3908cb8b86657cdaf71b020ed0115z26', 'Iron Assembly', 'Beastlord Darmac, Operator Thogar and Iron Maidens');
-INSERT INTO Forum (forumId, name, description) 
-	VALUES ('3908cb8b86657cdaf71b020ed0115z27', 'Blackhand', 'Blackhand');
-INSERT INTO Forum (forumId, name, description)
-	VALUES ('3908cb8b86657cdaf71b020ed0115z28', 'Hellbreach', 'Hellfire Assault, Iron Reaver and Kormrok');
-INSERT INTO Forum (forumId, name, description)
-	VALUES ('3908cb8b86657cdaf71b020ed0115z29', 'Halls of Blood', 'Hellfire High Council, Kilrogg Deadeye and Gorefiend');
-INSERT INTO Forum (forumId, name, description)
-	VALUES ('3908cb8b86657cdaf71b020ed0115z30', 'Bastion of Shadows', 'Shadow-Lord Iskar, Socrethar the Eternal and Tyrant Velhari');
-INSERT INTO Forum (forumId, name, description)
-	VALUES ('3908cb8b86657cdaf71b020ed0115z31', 'Destructor''s Rise', 'Fel Lord Zakuun, Xhul''horac and Mannoroth');
-INSERT INTO Forum (forumId, name, description)
-	VALUES ('3908cb8b86657cdaf71b020ed0115z32', 'The Black Gate', 'Archimonde');
+INSERT INTO Forum (forumId, name, description) 	VALUES ('3908cb8b86657cdaf71b020ed0115z21', 'The Walled City', 'Kargath, Butcher and Brackenspore');
+INSERT INTO Forum (forumId, name, description) 	VALUES ('3908cb8b86657cdaf71b020ed0115z22', 'Arcane Sanctum', 'Tectus, Twin Ogron and Ko\'ragh');
+INSERT INTO Forum (forumId, name, description) 	VALUES ('3908cb8b86657cdaf71b020ed0115z23', 'Imperator', 'Imperator Mor\'gok');
+INSERT INTO Forum (forumId, name, description) 	VALUES ('3908cb8b86657cdaf71b020ed0115z24', 'Slagworks', 'Oregorger, Gruul and Blast Furnace');
+INSERT INTO Forum (forumId, name, description) 	VALUES ('3908cb8b86657cdaf71b020ed0115z25', 'Black Forge', 'Hans and Franz, Flamebender Ka\'graz and Kromog');
+INSERT INTO Forum (forumId, name, description) 	VALUES ('3908cb8b86657cdaf71b020ed0115z26', 'Iron Assembly', 'Beastlord Darmac, Operator Thogar and Iron Maidens');
+INSERT INTO Forum (forumId, name, description) 	VALUES ('3908cb8b86657cdaf71b020ed0115z27', 'Blackhand', 'Blackhand');
+INSERT INTO Forum (forumId, name, description)	VALUES ('3908cb8b86657cdaf71b020ed0115z28', 'Hellbreach', 'Hellfire Assault, Iron Reaver and Kormrok');
+INSERT INTO Forum (forumId, name, description)	VALUES ('3908cb8b86657cdaf71b020ed0115z29', 'Halls of Blood', 'Hellfire High Council, Kilrogg Deadeye and Gorefiend');
+INSERT INTO Forum (forumId, name, description)	VALUES ('3908cb8b86657cdaf71b020ed0115z30', 'Bastion of Shadows', 'Shadow-Lord Iskar, Socrethar the Eternal and Tyrant Velhari');
+INSERT INTO Forum (forumId, name, description)	VALUES ('3908cb8b86657cdaf71b020ed0115z31', 'Destructor''s Rise', 'Fel Lord Zakuun, Xhul''horac and Mannoroth');
+INSERT INTO Forum (forumId, name, description)	VALUES ('3908cb8b86657cdaf71b020ed0115z32', 'The Black Gate', 'Archimonde');
