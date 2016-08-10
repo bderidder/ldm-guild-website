8e5b899f62614cfbbb74b3aca9a538538cebd8e7
Bavo De Ridder <bavo@coderspotting.org>
2016-08-10 08:32:27 +0200

    Added Demon Hunter class to the database

---
+++ app/DoctrineMigrations/Version20160810062728.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace LaDanseDomain\Migrations;
+
+use Doctrine\DBAL\Migrations\AbstractMigration;
+use Doctrine\DBAL\Schema\Schema;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+class Version20160810062728 extends AbstractMigration
+{
+    /**
+     * @param Schema $schema
+     */
+    public function up(Schema $schema)
+    {
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() != 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('INSERT INTO GameClass(id, name) VALUES (12, "Demon Hunter")');
+    }
+
+    /**
+     * @param Schema $schema
+     */
+    public function down(Schema $schema)
+    {
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() != 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('DELETE FROM GameClass WHERE id=12');
+    }
+}
