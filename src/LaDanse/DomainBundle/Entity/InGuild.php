c2746ec8aad0922ffe4873fd065b6ad5ccf473b8
Bavo De Ridder <bavo@coderspotting.org>
2016-09-04 09:03:48 +0200

    Added getter/setter

--- src/LaDanse/DomainBundle/Entity/InGuild.php
+++ src/LaDanse/DomainBundle/Entity/InGuild.php
@@ -45,4 +45,94 @@ class InGuild
      * @ORM\JoinColumn(name="character", referencedColumnName="id", nullable=false)
      */
     protected $character;
+
+    /**
+     * @return mixed
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param mixed $id
+     * @return InGuild
+     */
+    public function setId($id)
+    {
+        $this->id = $id;
+        return $this;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getFromTime()
+    {
+        return $this->fromTime;
+    }
+
+    /**
+     * @param mixed $fromTime
+     * @return InGuild
+     */
+    public function setFromTime($fromTime)
+    {
+        $this->fromTime = $fromTime;
+        return $this;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getEndTime()
+    {
+        return $this->endTime;
+    }
+
+    /**
+     * @param mixed $endTime
+     * @return InGuild
+     */
+    public function setEndTime($endTime)
+    {
+        $this->endTime = $endTime;
+        return $this;
+    }
+
+    /**
+     * @return Guild
+     */
+    public function getGuild(): Guild
+    {
+        return $this->guild;
+    }
+
+    /**
+     * @param Guild $guild
+     * @return InGuild
+     */
+    public function setGuild(Guild $guild): InGuild
+    {
+        $this->guild = $guild;
+        return $this;
+    }
+
+    /**
+     * @return Character
+     */
+    public function getCharacter(): Character
+    {
+        return $this->character;
+    }
+
+    /**
+     * @param Character $character
+     * @return InGuild
+     */
+    public function setCharacter(Character $character): InGuild
+    {
+        $this->character = $character;
+        return $this;
+    }
 }
