34a2c318118d233cb7d62f0ac2a6ef6e6930b6e5
Bavo De Ridder <bavo@coderspotting.org>
2016-09-14 21:54:24 +0200

    Merged two Guild(s)Resource controllers to one

--- src/LaDanse/RestBundle/Controller/Character/GuildsResource.php
+++ src/LaDanse/RestBundle/Controller/Character/GuildsResource.php
@@ -9,8 +9,11 @@ namespace LaDanse\RestBundle\Controller\Character;
 use LaDanse\RestBundle\Common\AbstractRestController;
 use LaDanse\RestBundle\Common\JsonResponse;
 use LaDanse\RestBundle\Common\ResourceHelper;
+use LaDanse\ServicesBundle\Common\ServiceException;
 use LaDanse\ServicesBundle\Service\Character\CharacterService;
+use LaDanse\ServicesBundle\Service\DTO\GameData\PatchGuild;
 use LaDanse\ServicesBundle\Service\DTO\Reference\StringReference;
+use LaDanse\ServicesBundle\Service\GameData\GameDataService;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Component\HttpFoundation\Request;
@@ -21,6 +24,24 @@ use Symfony\Component\HttpFoundation\Response;
  */
 class GuildsResource extends AbstractRestController
 {
+    /**
+     * @param Request $request
+     *
+     * @return Response
+     *
+     * @Route("", name="getAllGuilds")
+     * @Method({"GET"})
+     */
+    public function getAllGuilds(Request $request)
+    {
+        /** @var GameDataService $characterService */
+        $gameDataService = $this->get(GameDataService::SERVICE_NAME);
+
+        $guildsDto = $gameDataService->getAllGuilds();
+
+        return new JsonResponse(ResourceHelper::array($guildsDto));
+    }
+
     /**
      * @param Request $request
      * @param $guildId
@@ -40,5 +61,34 @@ class GuildsResource extends AbstractRestController
         );
 
         return new JsonResponse(ResourceHelper::array($characters));
+    }
+
+    /**
+     * @param Request $request
+     * @return Response
+     * @Route("", name="postGuild")
+     * @Method({"POST"})
+     */
+    public function postGuild(Request $request)
+    {
+        /** @var GameDataService $gameDataService */
+        $gameDataService = $this->get(GameDataService::SERVICE_NAME);
+
+        try
+        {
+            $patchGuild = $this->getDtoFromContent($request, PatchGuild::class);
+
+            $dtoGuild = $gameDataService->postGuild($patchGuild);
+
+            return new JsonResponse(ResourceHelper::object($dtoGuild));
+        }
+        catch(ServiceException $serviceException)
+        {
+            return ResourceHelper::createErrorResponse(
+                $request,
+                $serviceException->getCode(),
+                $serviceException->getMessage()
+            );
+        }
     }
 }
