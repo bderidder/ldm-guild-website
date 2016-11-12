0af56f27f7d98a4b68004ff8b11e65631b98c599
Bavo De Ridder <bavo@coderspotting.org>
2016-11-12 22:54:55 +0100

    Merged EventQueryController methods into EventsResource

--- src/LaDanse/RestBundle/Controller/Events/EventsResource.php
+++ src/LaDanse/RestBundle/Controller/Events/EventsResource.php
@@ -19,12 +19,69 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 
 /**
  * @Route("/")
  */
 class EventsResource extends AbstractRestController
 {
+    /**
+     * @param Request $request
+     *
+     * @return Response
+     *
+     * @Route("/", name="queryEvents")
+     * @Method({"GET","HEAD"})
+     */
+    public function queryEventsAction(Request $request)
+    {
+        /** @var EventService $eventService */
+        $eventService = $this->get(EventService::SERVICE_NAME);
+
+        /** @var \DateTime $startOnDate */
+        $startOnDate = $this->getStartOnDate($request->query->get('startOn'));
+
+        $eventPage = $eventService->getAllEventsPaged($startOnDate);
+
+        $pagedResult = [
+            'data'   => $eventPage->getEvents(),
+            'paging' => [
+                'previous' => $this->generateUrl(
+                    'queryEvents',
+                    ['startOn' => $eventPage->getPreviousTimestamp()->format('Ymd')],
+                    UrlGeneratorInterface::ABSOLUTE_URL
+                ),
+                'next'     => $this->generateUrl(
+                    'queryEvents',
+                    ['startOn' => $eventPage->getNextTimestamp()->format('Ymd')],
+                    UrlGeneratorInterface::ABSOLUTE_URL
+                )
+            ]
+        ];
+
+        return new JsonResponse($pagedResult);
+    }
+
+    /**
+     * @param Request $request
+     * @param int $eventId
+     *
+     * @return Response
+     *
+     * @Route("/{eventId}", name="queryEventById")
+     * @Method({"GET","HEAD"})
+     */
+    public function queryEventByIdAction(Request $request, $eventId)
+    {
+        /** @var EventService $eventService */
+        $eventService = $this->get(EventService::SERVICE_NAME);
+
+        $event = $eventService->getEventById($eventId);
+
+        return new JsonResponse($event);
+    }
+
     /**
      *
      * @ApiDoc(
@@ -59,5 +116,15 @@ class EventsResource extends AbstractRestController
                 $serviceException->getMessage()
             );
         }
+    }
+
+    private function getStartOnDate($pStartOnDate)
+    {
+        if ($pStartOnDate == null)
+        {
+            return new \DateTime();
+        }
+
+        return \DateTime::createFromFormat('Ymd', $pStartOnDate);
     }
 }
