868801c5c09e06df75cb280c3ed5ca940d869ea9
Bavo De Ridder <bavo@coderspotting.org>
2016-11-13 20:59:21 +0100

    Implemented PostSignUp DTO

--- src/LaDanse/ServicesBundle/Service/DTO/Event/PutSignUp.php
+++ src/LaDanse/ServicesBundle/Service/DTO/Event/PutSignUp.php
@@ -2,7 +2,60 @@
 
 namespace LaDanse\ServicesBundle\Service\DTO\Event;
 
+use JMS\Serializer\Annotation\SerializedName;
+use JMS\Serializer\Annotation\Type;
+use Symfony\Component\Validator\Constraints as Assert;
+
 class PutSignUp
 {
+    /**
+     * @var string
+     * @Type("string")
+     * @SerializedName("signUpType")
+     * @Assert\NotBlank()
+     */
+    private $signUpType;
 
+    /**
+     * @var array
+     * @Type("array<string>")
+     * @SerializedName("roles")
+     */
+    private $roles;
+
+    /**
+     * @return string
+     */
+    public function getSignUpType(): string
+    {
+        return $this->signUpType;
+    }
+
+    /**
+     * @param string $signUpType
+     * @return PutSignUp
+     */
+    public function setSignUpType(string $signUpType): PutSignUp
+    {
+        $this->signUpType = $signUpType;
+        return $this;
+    }
+
+    /**
+     * @return array
+     */
+    public function getRoles(): array
+    {
+        return $this->roles;
+    }
+
+    /**
+     * @param array $roles
+     * @return PutSignUp
+     */
+    public function setRoles(array $roles): PutSignUp
+    {
+        $this->roles = $roles;
+        return $this;
+    }
 }
