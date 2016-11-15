5dc4b1b14456ee2545d94ac398a772f3086b36f5
Bavo De Ridder <bavo@coderspotting.org>
2016-11-15 20:51:43 +0100

    Added forum for QLTalk addon

---
+++ app/DoctrineMigrations/Version20161115193441.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace LaDanseDomain\Migrations;
+
+use Doctrine\DBAL\Migrations\AbstractMigration;
+use Doctrine\DBAL\Schema\Schema;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+class Version20161115193441 extends AbstractMigration
+{
+    /**
+     * @param Schema $schema
+     */
+    public function up(Schema $schema)
+    {
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() != 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('INSERT INTO Forum (forumId, name, description) VALUES ("3908cb8b86657cdaf71b020ed0115z37", "QLTalk", "Discussions on LDM\'s chat addon")');
+    }
+
+    /**
+     * @param Schema $schema
+     */
+    public function down(Schema $schema)
+    {
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() != 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('DELETE FROM Forum WHERE forumId="3908cb8b86657cdaf71b020ed0115z37"');
+    }
+}
