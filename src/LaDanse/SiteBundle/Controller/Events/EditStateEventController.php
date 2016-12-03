774aad28921bd275918a65fa041ab2f64c2cf8dc
Bavo De Ridder <bavo@coderspotting.org>
2016-12-03 22:06:16 +0100

    Refactored to use new REST-like API for updating the state of an event

--- src/LaDanse/SiteBundle/Controller/Events/EditStateEventController.php
+++ src/LaDanse/SiteBundle/Controller/Events/EditStateEventController.php
@@ -3,7 +3,9 @@
 namespace LaDanse\SiteBundle\Controller\Events;
 
 use JMS\DiExtraBundle\Annotation as DI;
+use LaDanse\DomainBundle\FSM\EventStateMachine;
 use LaDanse\ServicesBundle\Service\Authorization\NotAuthorizedException;
+use LaDanse\ServicesBundle\Service\DTO\Event\PutEventState;
 use LaDanse\ServicesBundle\Service\Event\EventDoesNotExistException;
 use LaDanse\ServicesBundle\Service\Event\EventInvalidStateChangeException;
 use LaDanse\ServicesBundle\Service\Event\EventService;
@@ -38,7 +40,11 @@ class EditStateEventController extends LaDanseController
 
         try
         {
-            $eventService->confirmEvent($id);
+            $putEventState = new PutEventState();
+
+            $putEventState->setState(EventStateMachine::CONFIRMED);
+
+            $eventService->putEventState($id, $putEventState);
 
             return $this->redirect($this->generateUrl('viewEvent', ['id' => $id]));
         }
@@ -96,7 +102,11 @@ class EditStateEventController extends LaDanseController
 
         try
         {
-            $eventService->cancelEvent($id);
+            $putEventState = new PutEventState();
+
+            $putEventState->setState(EventStateMachine::CANCELLED);
+
+            $eventService->putEventState($id, $putEventState);
 
             return $this->redirect($this->generateUrl('viewEvent', ['id' => $id]));
         }
