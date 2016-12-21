762045f700bb5bb16bbf6ad8e1d203cb7152a939
Bavo De Ridder <bavo@bavoderidder.com>
2016-12-21 16:41:57 +0100

    Fixed a bug where the find methods had a faulty return path

--- src/LaDanse/ServicesBundle/Service/DTO/Event/Event.php
+++ src/LaDanse/ServicesBundle/Service/DTO/Event/Event.php
@@ -241,8 +241,6 @@ class Event
             /** @var SignUp $signUp */
             if ($signUp->getId() == $signUpId)
                 return $signUp;
-
-            return null;
         }
 
         return null;
@@ -255,8 +253,6 @@ class Event
             /** @var SignUp $signUp */
             if ($signUp->getAccount()->getId() == $accountId)
                 return $signUp;
-
-            return null;
         }
 
         return null;
