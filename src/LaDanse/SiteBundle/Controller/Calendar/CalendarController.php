3bff68c10404e4dacf41ca2086128ff595c1b8d5
Bavo De Ridder <bavo@bavoderidder.com>
2017-01-28 12:50:48 +0100

    Removed controller and twig templates that have been replaced with AngularJS and REST

--- src/LaDanse/SiteBundle/Controller/Calendar/CalendarController.php
+++ src/LaDanse/SiteBundle/Controller/Calendar/CalendarController.php
@@ -4,20 +4,10 @@ namespace LaDanse\SiteBundle\Controller\Calendar;
 
 use JMS\DiExtraBundle\Annotation as DI;
 use LaDanse\DomainBundle\Entity\Account;
-use LaDanse\ServicesBundle\Activity\ActivityEvent;
-use LaDanse\ServicesBundle\Activity\ActivityType;
 use LaDanse\ServicesBundle\Service\Event\EventService;
 use LaDanse\SiteBundle\Common\LaDanseController;
-use LaDanse\SiteBundle\Model\Calendar\CalendarDayModel;
-use LaDanse\SiteBundle\Model\Calendar\CalendarMonthModel;
-use LaDanse\SiteBundle\Model\Calendar\RaidWeekModel;
 use LaDanse\SiteBundle\Model\EventModel;
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
-
 use Symfony\Component\EventDispatcher\EventDispatcherInterface;
-use Symfony\Component\HttpFoundation\Request;
-
-use Symfony\Component\HttpFoundation\Response;
 
 class CalendarController extends LaDanseController
 {
@@ -37,157 +27,6 @@ class CalendarController extends LaDanseController
      */
     private $eventDispatcher;
 
-    /**
-     * @param $request Request
-     *
-     * @return Response
-     *
-     * @Route("/", name="calendarIndex")
-     */
-    public function indexAction(Request $request)
-    {
-        $authContext = $this->getAuthenticationService()->getCurrentContext();
-
-        if (!$authContext->isAuthenticated())
-        {
-            $this->logger->warning(__CLASS__ . ' the user was not authenticated in calendarIndex');
-
-            return $this->redirect($this->generateUrl('welcomeIndex'));
-        }
-
-        $this->eventDispatcher->dispatch(
-            ActivityEvent::EVENT_NAME,
-            new ActivityEvent(
-                ActivityType::CALENDAR_VIEW,
-                $this->getAuthenticationService()->getCurrentContext()->getAccount())
-        );
-
-        $showDateStr = $request->query->get('showDate');
-
-        if ($showDateStr === null)
-        {
-            $showDate = new \DateTime('today');
-        }
-        else
-        {
-            $showDate = \DateTime::createFromFormat(CalendarController::QUERY_DATE_FORMAT, $showDateStr);
-
-            if ($showDate === false)
-            {
-                $showDate = new \DateTime('today');
-            }
-        }
-
-        return $this->render('LaDanseSiteBundle:calendar:calendar.html.twig',
-            ['showDate' => $showDate]
-        );
-    }
-
-    /**
-     * @param $request Request
-     *
-     * @return Response
-     *
-     * @Route("/test", name="testCalendarAction")
-     */
-    public function testCalendarAction(Request $request)
-    {
-        return $this->indexPartialAction(\DateTime::createFromFormat("Y-m-d", "2016-10-16"));
-    }
-
-    public function indexPartialAction(\DateTime $showDate)
-    {
-        /** @var CalendarMonthModel $calendarMonthModel */
-        $calendarMonthModel = new CalendarMonthModel($showDate);
-
-        // create a RaidWeekModel representing the current raid week
-        /** @var RaidWeekModel $raidWeek */
-        $raidWeek = new RaidWeekModel(new \DateTime());
-
-        if (!$calendarMonthModel->containsDate($raidWeek->getFirstDate())
-            &&
-            $calendarMonthModel->containsDate($raidWeek->getLastDate()))
-        {
-            $calendarMonthModel->shiftOneWeekBack();
-        }
-
-        /* @var \DateTime $startDate */
-        $startDate = $calendarMonthModel->getStartDate();
-
-        // the algoritm below needs to start on the day before, so we substract a day
-        $startDate = $startDate->modify('-1days');
-
-        $calendarDates = [];
-
-        $events = $this->getEvents($startDate, $this->getAuthenticationService()->getCurrentContext()->getAccount());
-
-        $eventIndex = 0;
-
-        $currentDate = clone $startDate;
-
-        // we show 28 days, that is 4 weeks
-        for($i = 0; $i < 28; $i++)
-        {
-            $date = clone $currentDate;
-
-            $date->modify('+1days');
-
-            $calendarDateModel = new CalendarDayModel($date);
-
-            $calendarDateModel->setIsInCurrentRaidWeek($raidWeek->inRaidWeek($date));
-
-            // show the month in the first cell (first row, first day)
-            if ($i === 0) $calendarDateModel->setShowMonth(true);
-
-            // show the month if we change month
-            if ($date->format("d") === "01") $calendarDateModel->setShowMonth(true);
-
-            $calendarDates[] = $calendarDateModel;
-
-            while($eventIndex < count($events))
-            {
-                /* @var $currentEvent EventModel */
-                $currentEvent = $events[$eventIndex];
-                $inviteTime = $currentEvent->getInviteTime();
-
-                if (($date->format(CalendarController::COMPARE_DATE_FORMAT) 
-                    < $inviteTime->format(CalendarController::COMPARE_DATE_FORMAT)))
-                {
-                    break;
-                }
-                elseif ($date->format(CalendarController::COMPARE_DATE_FORMAT) 
-                    == $inviteTime->format(CalendarController::COMPARE_DATE_FORMAT))
-                {
-                    $calendarDateModel->addEvent($currentEvent);
-
-                    $eventIndex++;
-                }
-                else
-                {
-                    $eventIndex++;
-                }
-            }
-
-            $currentDate = $date;
-        }
-
-        $previousPageDate = clone $startDate;
-        $previousPageDate->sub(new \DateInterval('P21D'));
-
-        $nextPageDate = clone $startDate;
-        $nextPageDate->add(new \DateInterval('P35D'));
-
-        $todayDate = new \DateTime('today');
-
-        return $this->render('LaDanseSiteBundle:calendar:calendarPartial.html.twig',
-                [
-                    'calendarDays'     => $calendarDates,
-                    'previousPageDate' => $previousPageDate->format(CalendarController::QUERY_DATE_FORMAT),
-                    'todayPageDate'    => $todayDate->format(CalendarController::QUERY_DATE_FORMAT),
-                    'nextPageDate'     => $nextPageDate->format(CalendarController::QUERY_DATE_FORMAT)
-                ]
-        );
-    }
 
     public function tilePartialAction()
     {
