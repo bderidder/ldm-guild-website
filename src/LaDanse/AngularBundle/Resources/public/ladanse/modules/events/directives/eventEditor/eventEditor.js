12c10e3ba4840dcce19fc816104adc83ea8a8741
Bavo De Ridder <bderidder@gmail.com>
2018-08-10 20:04:53 +0200

    Added Uldir raid for Battle for Azeroth

--- src/LaDanse/AngularBundle/Resources/public/ladanse/modules/events/directives/eventEditor/eventEditor.js
+++ src/LaDanse/AngularBundle/Resources/public/ladanse/modules/events/directives/eventEditor/eventEditor.js
@@ -35,7 +35,7 @@ eventsModule.directive('eventEditor', function()
     ctrl.formIsValid = false;
 
     ctrl.masterData = {
-        raidInstances: ['The Emerald Nightmare', 'Trial of Valor', 'The Nighthold', 'Tomb of Sargeras', 'Antorus, the Burning Throne'],
+        raidInstances: ['The Emerald Nightmare', 'Trial of Valor', 'The Nighthold', 'Tomb of Sargeras', 'Antorus, the Burning Throne', 'Uldir'],
         instanceDifficulties: ['To be decided', 'Normal', 'Heroic', 'Mythic']
     };
 
